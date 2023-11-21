123967e2aa34f26bd31346f2ca265da31a472d45
silviastoykova <sylvia.stoykova@gmail.com>
2023-11-21 13:58:41 +0200

    Add constants.js file with constant variables
    File is used to store constant variables to be used in the project code.
    
    The variables prevent using magic numbers when validating data.

---
+++ collab-messenger/src/common/constants.js
@@ -0,0 +1,23 @@
+export const ZERO = 0;
+
+export const MIN_USERNAME_LENGHT = 5;
+
+export const MAX_USERNAME_LENGTH = 35;
+
+export const MIN_PASSWORD_LENGTH = 6;
+
+export const MIN_NAME_LENGTH = 1;
+
+export const MAX_NAME_LENGTH = 32;
+
+export const MIN_CHANNEL_NAME_LENGTH =3;
+
+export const MAX_CHANNEL_NAME_LENGTH = 40;
+
+export const MIN_TEAM_NAME_LENGTH = 3;
+
+export const MAX_TEAM_NAME_LENGTH = 40;
+
+export const MIN_MESSAGE_LENGTH = 1;
+
+export const MAX_MESSAGE_LENGTH = 4000;
