3b581bc0805fc8d05ba13b4311a7496bf41ef896
georgiDobrev <georgidobrev.5@gmail.com>
2023-11-21 20:52:08 +0200

    Add AuthenticatedRoute.js

---
+++ collab-messenger/src/hoc/AuthenticatedRoute.js
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import AppContext from '../context/AuthContext';
+import { Navigate, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function AuthenticatedRoute({ children }) {
+  const { user } = useContext(AppContext);
+  const location = useLocation();
+
+  if (user === null) {
+    return (
+      <Navigate to="/" path={location.pathname}>
+        {' '}
+      </Navigate>
+    );
+  }
+
+  return children;
+}
+
+AuthenticatedRoute.propTypes = {
+  children: PropTypes.node,
+};
