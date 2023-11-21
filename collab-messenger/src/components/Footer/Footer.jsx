446bce73d54749ba91b459ef0ba317ba5667f0d6
silviastoykova <sylvia.stoykova@gmail.com>
2023-11-21 14:05:32 +0200

    Add Footer component

---
+++ collab-messenger/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="bottom-0">
+      <br />
+      <hr className="border-gray-600"/>
+      <br />
+      <footer>&copy; Assemble 2023</footer>
+      <br />
+    </div>
+  )
+}
