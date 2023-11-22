3f7b8e02a5467523fc74712485efc1cf0ed8ec6d
georgiDobrev <georgidobrev.5@gmail.com>
2023-11-23 00:16:46 +0200

    Modify tailwind.config.cjs
    - Added primary colors.

--- collab-messenger/tailwind.config.cjs
+++ collab-messenger/tailwind.config.cjs
@@ -1,10 +1,20 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
       'light-gray': '#EFEFF0',
-      red: '#ff0000'
+      red: '#ff0000',
+      white: '#F9F9FF',
+      grey: '#EEEFF5',
+      yellow: '#FFD100',
+      mint: '#77EAE2',
+      blue: '#0075FF',
+      'darker-blue': '#0C12D7',
+      pink: '#EE4FE8',
+      black: '#000C1A',
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
 };
