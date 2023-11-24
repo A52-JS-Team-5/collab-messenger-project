9472ed91929a5a241047a68749a3ca494175a744
georgiDobrev <georgidobrev.5@gmail.com>
2023-11-24 14:39:00 +0200

    Modify Login.jsx
    - Added link to redirect to register page.

--- collab-messenger/src/views/Login/Login.jsx
+++ collab-messenger/src/views/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import AppContext from "../../context/AuthContext.js";
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { loginUser } from "../../services/auth.services.js";
 import { getUserByHandle } from "../../services/users.services.js";
 import { toast } from 'react-toastify';
@@ -75,7 +76,7 @@ const Login = () => {
       <div className="hero-content flex-col lg:flex-row-reverse pt-20 pb-20 gap-16 max-2xl:gap-8">
         <div className='card rounded border-2 border-black rounded-3xl bg-beige shadow-xl min-w-[50%]'>
           <div className='form-control flex flex-wrap flex-auto'>
-            <h3 className="text-3xl self-center text-black pb-8">Log In</h3>
+            <h3 className="text-3xl self-center text-black pb-8 font-bold">Sign In</h3>
             <label className="label" htmlFor='username'>
               <span className="label-text text-black pl-3">Username</span>
             </label>
@@ -87,7 +88,11 @@ const Login = () => {
             <input type='password' id='password' placeholder="Type your password" className="input input-bordered w-full bg-white" value={form.password} onChange={updateForm('password')} />
             <span className="err-message text-red">{formErrorMsg.password}</span>
             <br />
-            <button className="btn bg-black self-center text-white w-full" onClick={onLogin}>Login</button>
+            <div className="text-left pb-6 pl-3 flex space-x-24">
+              <p>New to Chatter?</p>
+              <Link to='/login'>Create an account</Link>
+            </div>
+            <button className="btn bg-black self-center text-white w-full" onClick={onLogin}>Sign In</button>
           </div>
         </div>
       </div>
