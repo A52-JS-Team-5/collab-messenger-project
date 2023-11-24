9bb56e9456435de24d3dac285ca1b7986ecaa399
silviastoykova <sylvia.stoykova@gmail.com>
2023-11-24 17:52:27 +0200

    Add reaction to msg option

--- collab-messenger/src/components/MessageBubble/MessageBubble.jsx
+++ collab-messenger/src/components/MessageBubble/MessageBubble.jsx
@@ -1,12 +1,15 @@
 import Avatar from '../Avatar/Avatar';
 import { editMessage } from '../../services/messages.services';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import QuickReactions from "react-quick-reactions";
 
 export default function MessageBubble({ message, messageClass, userAvatar, editCommentOption }) {
   const [form, setForm] = useState('');
   const [showComment, setShowComment] = useState(true);
+  const [areEmojisVisible, setAreEmojisVisible] = useState(false);
   const [originalContent, setOriginalContent] = useState(message.content);
+  const [reactions, setReactions] = useState(message?.reactions || []);
   const isGif = message.content.includes('giphy');
 
   const onEdit = () => {
@@ -19,7 +22,7 @@ export default function MessageBubble({ message, messageClass, userAvatar, editC
   }
 
   const setNewContent = () => {
-    editMessage(message.id, { content: form })
+    editMessage(message.id, { content: form });
     setOriginalContent(form);
     setShowComment(true);
   }
@@ -34,30 +37,142 @@ export default function MessageBubble({ message, messageClass, userAvatar, editC
     }
   };
 
+  const onReaction = (emoji) => {
+    if (reactions.includes(emoji) === false) {
+      setReactions(prevReactions => [...prevReactions, emoji]);
+    }
+  };
+
+  useEffect(() => {
+    editMessage(message.id, { reactions: reactions });
+  }, [reactions, message.id]);
+
   return (
     <div id='chat-wrapper' className={`chat ${messageClass}`} >
       <Avatar user={userAvatar}/>
-      <div className="chat-header gap-2">
+      <div className="chat-header ">
         {message.author}
+        <time className="text-xs opacity-50 pl-1">{new Date(message.createdOn).toLocaleString()}</time>
       </div>
-      {showComment === false ? (
+
+      {showComment === false ? ( // is edit button clicked on?
+        // if yes -> show input
         <div key={message.id} className='flex space-between'>
           <input type="text" value={`${form}`} onChange={onInputChange} onKeyDown={handleKeyDown} className="input input-bordered input-xs w-full max-w-xs" />
           <button className="btn btn-ghost btn-xs flex self-center text-white" onClick={setNewContent}>Save</button>
         </div>
       ) : (
+        // if no -> check if msg is gif
         isGif === true ? (
-          <img
-            src={message.content}
-            alt="GIF"
-          />
+          // if yes -> display gif
+          <div className='flex flex-row'>
+            <img
+              src={message.content}
+              alt="GIF"
+            />
+            {/* edit option button */}
+            {editCommentOption && (<div className="flex self-start pr-2 pt-0.5 text-xs opacity-50 hover:cursor-pointer pl-2" onClick={onEdit}><i className="fa-solid fa-pen-to-square"></i></div>)}
+            {/* react option button -> can only react on other user's posts */}
+            {editCommentOption !== true && (
+              <div className='flex flex-row'>
+                <div className="flex self-center pl-1 text-xs hover:cursor-pointer">
+                  <QuickReactions
+                    reactionsArray={[
+                      {
+                        id: "laughing",
+                        name: "Laughing",
+                        content: "😂",
+                      },
+                      {
+                        id: "crying",
+                        name: "Crying",
+                        content: "😢",
+                      },
+                      {
+                        id: "heart",
+                        name: "Heart",
+                        content: "❤️",
+                      },
+                      {
+                        id: "thumbs-up",
+                        name: "Thumbs Up",
+                        content: "👍",
+                      },
+                      {
+                        id: "thumbs-down",
+                        name: "Thumbs Down",
+                        content: "👎",
+                      },
+                    ]}
+                    placement='right'
+                    isVisible={areEmojisVisible}
+                    onClose={() => setAreEmojisVisible(false)}
+                    onClickReaction={(reaction) => {
+                      onReaction(reaction.content)
+                    }}
+                    trigger={
+                      <div className='flex self-center' onClick={() => setAreEmojisVisible(!areEmojisVisible)}><i className="fa-regular fa-face-smile opacity-50"></i></div>
+                    }
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         ) : (
-          <div className="chat-bubble">{message.content}</div>
+          // if it's is not a gif, show message content 
+          <div className='flex flex-row'>
+            {editCommentOption && (<div className="flex self-center pr-2 pt-0.5 text-xs opacity-50 hover:cursor-pointer pl-2" onClick={onEdit}><i className="fa-solid fa-pen-to-square"></i></div>)}
+            <div className="chat chat-bubble">{message.content}</div>
+            {editCommentOption !== true && (
+              <div className='flex flex-row'>
+                <div className="flex self-center pl-1 text-xs hover:cursor-pointer">
+                  <QuickReactions
+                    reactionsArray={[
+                      {
+                        id: "laughing",
+                        name: "Laughing",
+                        content: "😂",
+                      },
+                      {
+                        id: "crying",
+                        name: "Crying",
+                        content: "😢",
+                      },
+                      {
+                        id: "heart",
+                        name: "Heart",
+                        content: "❤️",
+                      },
+                      {
+                        id: "thumbs-up",
+                        name: "Thumbs Up",
+                        content: "👍",
+                      },
+                      {
+                        id: "thumbs-down",
+                        name: "Thumbs Down",
+                        content: "👎",
+                      },
+                    ]}
+                    placement='right'
+                    isVisible={areEmojisVisible}
+                    onClose={() => setAreEmojisVisible(false)}
+                    onClickReaction={(reaction) => {
+                      onReaction(reaction.content)
+                    }}
+                    trigger={
+                      <div className='flex self-center' onClick={() => setAreEmojisVisible(!areEmojisVisible)}><i className="fa-regular fa-face-smile opacity-50"></i></div>
+                    }
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         )
       )}
+
       <div className="chat-footer flex">
-        {editCommentOption && (<div className="flex self-start pr-2 pt-0.5 text-xs opacity-50 hover:cursor-pointer" onClick={onEdit}><i className="fa-solid fa-pen-to-square"></i></div>)}
-        <time className="text-xs opacity-50 flex self-end ">{new Date(message.createdOn).toLocaleDateString()}</time>
+        {reactions?.length > 0 && <div className='p-0.5'>{reactions}{reactions.length}</div>}
       </div>
     </div>
   )
