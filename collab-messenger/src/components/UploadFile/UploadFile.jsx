c7edb0315f9c97b3d4da6442eec64b95b591829f
silviastoykova <sylvia.stoykova@gmail.com>
2023-11-27 16:25:55 +0200

    Streamline File Upload Handling
    Removed submit button for file upload
    
    Modified handleFileUpload function to trigger when a file is selected

--- collab-messenger/src/components/UploadFile/UploadFile.jsx
+++ collab-messenger/src/components/UploadFile/UploadFile.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
@@ -9,23 +9,20 @@ import { addMessage, createFileUploadMessage } from '../../services/messages.ser
 import { updateChatFiles } from "../../services/chats.services";
 
 export default function UploadFile({ message, setMessageFunc }) {
-  const [selectedFile, setSelectedFile] = useState('');
+  // const [selectedFile, setSelectedFile] = useState('');
   const loggedUser = useContext(AppContext);
   const storage = getStorage();
   const { chatId } = useParams();
 
-  const handleFileUpload = () => {
-    if (selectedFile === '') {
-      return;
-    }
+  const handleFileUpload = (file) => {
 
-    const storageRef = ref(storage, `chat_uploads/${chatId}/${selectedFile.name}`);
-    uploadBytes(storageRef, selectedFile)
+    const storageRef = ref(storage, `chat_uploads/${chatId}/${file.name}`);
+    uploadBytes(storageRef, file)
       .then(() => {
         return getDownloadURL(storageRef);
       })
       .then((downloadURL) => {
-        createFileUploadMessage(downloadURL, loggedUser.userData.handle, chatId, selectedFile.name, selectedFile.type)
+        createFileUploadMessage(downloadURL, loggedUser.userData.handle, chatId, file.name, file.type)
           .then((messageId) => {
             return addMessage(loggedUser.userData.handle, chatId, messageId, downloadURL)
           })
@@ -51,9 +48,9 @@ export default function UploadFile({ message, setMessageFunc }) {
   return (
     <div className="dropdown dropdown-right dropdown-end ">
       <label tabIndex={0} className="btn hover:cursor-pointer hover:bg-mint btn btn-sm text-black bg-transparent flex items-center w-fit"><i className="fa-solid fa-paperclip"></i></label>
-      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 bg-mint">
-        <li><a><input type="file" className='text-black' accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf" onChange={(event) => {setSelectedFile(event.target.files[0])}}/></a></li>
-        <li><a><input type="submit" placeholder='Submit' onClick={handleFileUpload} className='hover:cursor-pointer text-black'/></a></li>
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 bg-light-gray">
+        <li><a><input type="file" className='text-black' accept=".jpeg,.jpg,.gif,.pdf" onChange={(event) => {handleFileUpload(event.target.files[0]);}}/></a></li>
+        {/* <li><a><input type="submit" placeholder='Submit' onClick={handleFileUpload} className='hover:cursor-pointer text-black'/></a></li> */}
       </ul>
     </div>
   )
