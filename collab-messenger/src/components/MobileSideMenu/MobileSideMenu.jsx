bd0d4b321e61d78ed6aeabc261103a8cd35b25e9
Teodora Gigova <gigova.teodora@gmail.com>
2023-11-28 15:50:09 +0200

    Remove comments in MobileSideMenu
    - Removed unnecessary comments for cleaner code.

--- collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
+++ collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
@@ -12,23 +12,6 @@ const MobileSideMenu = () => {
 
     return (
         <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-lightBlue rounded-full bottom-4 left-1/2 lg:hidden sm:max-lg:display-block">
-            {/*
-        <div className="btm-nav btm-nav-lg md:hidden bg-lightBlue sm:max-md:display-block">
-            <button onClick={() => handleLinkClick('/app')} className={`bg-lightBlue border-lightBlue text-blue ${activeLink === '/app' ? 'active' : 'hover:!text-blue hover:bg-lightBlue focus:!bg-blue30'}`}>
-                    <i className="fa-solid fa-house"></i>
-                    <p className='text-xs'>Insights</p>
-
-            </button>
-            <button onClick={() => handleLinkClick('/app/chats')} className={`bg-lightBlue border-lightBlue text-blue ${activeLink === '/app/chats' ? 'active' : 'hover:!text-blue hover:bg-lightBlue focus:!bg-blue30'}`}>
-                    <i className="fa-solid fa-message"></i>
-                    <p className='text-xs'>Chats</p>
-            </button>
-            <button onClick={() => handleLinkClick('/app/teams')} className={`bg-lightBlue border-lightBlue text-blue ${activeLink === '/app/teams' ? 'active' : 'hover:!text-blue hover:bg-lightBlue focus:!bg-blue30'}`}>
-                    <i className="fa-solid fa-users"></i>
-                    <p className='text-xs'>Teams</p>
-            </button>
-        </div>
-    */}
             <div className="grid h-full grid-cols-3">
                 <button onClick={() => handleLinkClick('/app')} type="button" className={`${activeLink === '/app' && 'bg-blue text-white'} inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group text-blue`}>
                     <i className="fa-solid fa-house"></i>
@@ -44,7 +27,6 @@ const MobileSideMenu = () => {
                 </button>
             </div>
         </div>
-
     )
 }
 
