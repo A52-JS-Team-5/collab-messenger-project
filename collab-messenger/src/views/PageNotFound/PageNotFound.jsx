886f3c9c5ce8fc14f14b5acba616852ba22084a7
Teodora Gigova <gigova.teodora@gmail.com>
2023-11-29 21:19:05 +0200

    Refactor and style PageNotFound
    -Introduced PageNotFound.svg.
    - Updated styling and added logic for a Call to Action (CTA).

--- collab-messenger/src/views/PageNotFound/PageNotFound.jsx
+++ collab-messenger/src/views/PageNotFound/PageNotFound.jsx
@@ -1,15 +1,26 @@
-import { useNavigate } from 'react-router-dom';
-import PageNotFoundImage from '../../assets/page-not-found/PageNotFound.png'
+import { useLocation, useNavigate } from 'react-router-dom';
+import PageNotFoundImage from '../../assets/page-not-found/PageNotFound.svg'
+import { useEffect, useState } from 'react';
 
 export default function PageNotFound() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState('');
+
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
 
   return (
-    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12'>
-      <img src={PageNotFoundImage} id='PageNotFound' alt="Page Not Found Illustration" width={200} />
-      <h1> Page Not Found </h1>
+    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center'>
+      <img src={PageNotFoundImage} id='PageNotFound' alt="Page Not Found Illustration" className='max-h-64' />
+      <h1 className='text-3xl font-bold'> Page Not Found </h1>
       <p> We&apos;re sorry, the page you requested could not be found.</p>
-      <button onClick={() => navigate('/')}>Return to Homepage</button>
+      <div className='flex flex-row gap-2'>
+        {!activeLink.includes('/app') && <button onClick={() => navigate('/')} className='btn bg-pink border-none text-pureWhite'>Go to to Homepage</button>}
+        {activeLink.includes('/app') && <button onClick={() => navigate('/app')} className='btn bg-pink border-none text-pureWhite'>Go to Insights</button>}
+      </div>
     </div>
   )
 }
