53e265b2ee9be4d3b5fc36e63f9791e063a05d69
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-02 16:34:46 +0200

    Introduce StatusBubble component
    Moved implementation of the view of user's status to new component for improved readability

---
+++ collab-messenger/src/components/StatusBubble/StatusBubble.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { getUserByHandle } from "../../services/users.services";
+import PropTypes from 'prop-types';
+import { db } from '../../config/firebase-config';
+import { ref, onValue } from 'firebase/database';
+
+export default function StatusBubble({ view, userHandle }) {
+  const [userCurrentStatus, setUserCurrentStatus] = useState('');
+  const statusColors = {
+    'Online': 'bg-green',
+    'Offline': 'bg-black',
+    'Away': 'bg-yellow'
+  };
+
+  const positionOfStatus = {
+    'ChatDetails': 'left-14 top-10',
+    'ChatBox': 'left-8 top-10'
+  }
+
+  useEffect(() => {
+    getUserByHandle(userHandle)
+      .then((userData) => {
+        setUserCurrentStatus(userData?.status);
+      })
+      .catch((error) => {
+        console.error('Error fetching user status: ', error);
+      })
+
+  }, [userHandle]);
+
+  useEffect(() => {
+    const userRef = ref(db, `users/${userHandle}/status`);
+    const userStatusListener = onValue(userRef, (snapshot) => {
+      const updatedStatus = snapshot.val();
+      setUserCurrentStatus(updatedStatus);
+    });
+
+    return () => {
+      userStatusListener();
+    };
+  }, [userHandle])
+
+  return <div className={`absolute w-2 h-2 rounded-full ${positionOfStatus[view]} ${statusColors[userCurrentStatus]}`}></div>
+}
+
+StatusBubble.propTypes = {
+  view: PropTypes.string,
+  userHandle: PropTypes.string
+}
