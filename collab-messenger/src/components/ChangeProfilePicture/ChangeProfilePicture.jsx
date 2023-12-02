a749ace1c02955434c04d2b3eae5d3c3af7c833d
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-02 17:40:53 +0200

    Update & style ChangeProfilePicture
    - Improved input errors, replaced alerts with toasts.
    - Updated styling to match the app design.
    - Removed ChangeProfile.css.

--- collab-messenger/src/components/ChangeProfilePicture/ChangeProfilePicture.jsx
+++ collab-messenger/src/components/ChangeProfilePicture/ChangeProfilePicture.jsx
@@ -1,33 +1,34 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import './ChangeProfilePicture.css';
 import { useState, useEffect } from 'react';
 import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
 import { editUserProfile } from "../../services/users.services";
 import PropTypes from 'prop-types';
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function ChangeProfilePicture({ handle }) {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [isUpdating, setIsUpdating] = useState(false);
-  const [updateError, setUpdateError] = useState(null);
-  const [photoURL, setPhotoURL] = useState('https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg')
-  const [isSuccessful, setIsSuccessful] = useState(false);
   const allowedTypes = ['image/jpeg', 'image/png'];
-
+  const [fileErrorMsg, setFileErrorMsg] = useState('');
   const auth = getAuth();
   const storage = getStorage();
+  const [photoURL, setPhotoURL] = useState('https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg');
 
   useEffect(() => {
     if (auth?.currentUser.photoURL) {
       setPhotoURL(auth.currentUser.photoURL);
     }
-  }, [auth])
+  }, [auth?.currentUser.photoURL])
 
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
 
-    if (!allowedTypes.includes(file.type)) {
-      alert('Invalid file type. Please upload a JPEG, PNG, or PDF file.');
-      document.getElementById('fileInput').value = '';
+    if (!allowedTypes.includes(file.type) && file) {
+      setFileErrorMsg('Invalid file type. Please upload a JPEG, JPG or PNG file.');
+      document.getElementById('fileInput').value = null;
+    } else {
+      setFileErrorMsg('');
     }
 
     setSelectedPhoto(file);
@@ -35,11 +36,11 @@ export default function ChangeProfilePicture({ handle }) {
 
   const handleUpdatePhoto = () => {
     if (!selectedPhoto) {
+      setFileErrorMsg('Please select a JPEG, JPG or PNG file.');
       return; // No file selected
     }
 
     setIsUpdating(true);
-    setUpdateError(null);
 
     // Upload the selected photo to Firebase Storage
     const storageRef = ref(storage, `profile_photos/${auth.currentUser.uid}/${selectedPhoto.name}`);
@@ -55,48 +56,47 @@ export default function ChangeProfilePicture({ handle }) {
       })
       .then((downloadURL) => {
         setIsUpdating(false);
-        setIsSuccessful(true);
-        // Profile photo updated successfully
 
         // Now that we have the downloadURL, we can update the Realtime Database
         return editUserProfile(handle, { photoURL: downloadURL });
       })
+      .then(result => {
+        if (result === 'Successful update') {
+          setIsUpdating(false);
+          // Photo updated successfully
+          toast('Profile picture updated successfully.');
+        }
+      })
       .catch((error) => {
         setIsUpdating(false);
-        setUpdateError(error.message);
+        console.log(error.message);
+        toast('An error occurred while trying to update the team photo. Please try again.');
       });
   };
 
   return (
-    <div className="update-picture flex flex-col gap-4">
-      <div className='picture-details'>
-        <div className='picture-intro'>
-          <h2>Update Profile Picture</h2>
-          <p>A picture helps people recognize you and lets you know when you are signed in to your account.</p>
+    <div className="flex flex-col gap-4 basis-4/5 rounded-md bg-pureWhite p-4 h-full">
+      <div className='flex flex-col gap-4 rounded-md border-lightBlue border-2 p-8 w-full'>
+        <div className='flex flex-col items-start'>
+          <h3 className='font-bold text-lg text-left'>Update Profile Photo</h3>
+          <p className="text-left">A picture helps people recognize you and lets you know when you are signed in to your account.</p>
         </div>
-        <div className="picture-form-control flex flex-row gap-4 max-md:flex-col">
-          <img src={photoURL} alt="Avatar" className="avatar" />
-          <div className="flex flex-col justify-center">
+        <div className="flex flex-row gap-4 max-md:flex-col items-center w-full">
+          <div className='flex flex-col self-stretch w-24 justify-top'>
+            <img src={photoURL} alt="Profile Photo" className="object-cover rounded-md" />
+          </div>
+          <div className="flex flex-col justify-center w-full">
             <label className="label self-stretch">
-              <span className="label-text">Pick a Profile Picture</span>
+              <span className="label-text">Pick a Profile Photo</span>
             </label>
-            <input type="file" className="file-input file-input-bordered w-full flex-start self-stretch bg-light-gray" accept="image/*" id="fileInput" onChange={handlePhotoChange} />
-              {selectedPhoto && selectedPhoto !== '' && (<button className="btn btn-active btn-secondary" onClick={handleUpdatePhoto}>{isUpdating ? "Updating..." : "Update Photo"}</button>)}
+            <input type="file" className="file-input file-input-bordered w-full flex-start self-stretch bg-white file:bg-pink file:border-pink file:text-pureWhite" accept="image/*" id="fileInput" onChange={handlePhotoChange} />
+            {fileErrorMsg && <span className="err-message text-red">{fileErrorMsg}</span>}
+          </div>
+          <div className='flex flex-col self-stretch justify-end'>
+            {selectedPhoto && !fileErrorMsg && (<button className="btn border-none bg-pink text-pureWhite w-32" onClick={handleUpdatePhoto}>{isUpdating ? "Updating..." : "Update Photo"}</button>)}
           </div>
         </div>
       </div>
-      {updateError && (
-        <div className="alert alert-error">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          <span>{updateError}</span>
-        </div>
-      )}
-      {isSuccessful && (
-        <div className="alert alert-success">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          <span>Profile Picture Updated Successfully.</span>
-        </div>
-      )}
     </div>
   );
 }
