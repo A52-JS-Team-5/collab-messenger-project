34f7e886e6a9a7363db06765ee4a92d14ccc4337
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-02 16:32:54 +0200

    Enhance Avatar component for various views
    Adapted Avatar's presentation based on the specific view it is used in
    
    Introduced new variables to enhance code readability and maintainability

--- collab-messenger/src/components/Avatar/Avatar.jsx
+++ collab-messenger/src/components/Avatar/Avatar.jsx
@@ -2,27 +2,34 @@ import { useEffect, useState, useContext } from 'react';
 import { getUserByHandle } from '../../services/users.services';
 import PropTypes from 'prop-types';
 import AppContext from '../../context/AuthContext';
+import { DEFAULT_USER_PHOTO } from '../../common/constants';
 
-export default function Avatar({ user }) {
-  const [userAvatar, setUserAvatar] = useState('https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg');
+export default function Avatar({ user, chatComponent='' }) {
+  const [userAvatar, setUserAvatar] = useState(DEFAULT_USER_PHOTO);
   const loggedUser = useContext(AppContext);
+  const loggedUserHandle = loggedUser.userData?.handle;
+  const loggedUserAvatar = loggedUser?.userData?.photoURL;
 
   useEffect(() => {
-    if (loggedUser.userData?.handle !== user) {
+    if (loggedUserHandle !== user) {
       getUserByHandle(user)
         .then((userData) => {
-          setUserAvatar(userData.val()?.photoURL);
+          if(userData.photoURL){
+            setUserAvatar(userData.photoURL);
+          }
         })
         .catch((error) => {
           console.error('Error fetching avatar: ', error);
         })
     } else {
-      setUserAvatar(loggedUser?.userData?.photoURL)
+      if (loggedUserAvatar) {
+        setUserAvatar(loggedUserAvatar);
+      }
     }
-  }, [user, loggedUser?.userData?.handle, loggedUser?.userData?.photoURL]);
+  }, [user, loggedUserHandle, loggedUserAvatar]);
 
   return (
-    <div className="chat-image avatar w-10 h-10">
+    <div className={`chat-image static avatar ${chatComponent ? 'w-23 h-23' : 'w-10 h-10'}`}>
       <div className="rounded-full">
         <img src={userAvatar} />
       </div>
@@ -31,5 +38,6 @@ export default function Avatar({ user }) {
 }
 
 Avatar.propTypes = {
-  user: PropTypes.string
+  user: PropTypes.string,
+  chatComponent: PropTypes.bool
 }
