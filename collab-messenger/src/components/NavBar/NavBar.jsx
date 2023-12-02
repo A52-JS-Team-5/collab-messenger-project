36f2fa5e666b64a7b9ebda0f21ddd115f6783499
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-02 17:52:34 +0200

    Update NavBar
    - Improved logic for user profile photo retrieval.
    - Removed unnecessary code.
    - Refined links.

--- collab-messenger/src/components/NavBar/NavBar.jsx
+++ collab-messenger/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { useContext } from 'react';
-// import AppContext from '../../context/AuthContext';
 import './NavBar.css';
 import icon from '/icon.png'
+import AppContext from '../../context/AuthContext';
 
-export default function NavBar({ user, onLogout, loggedUserHandle }) {
-  // const [searchTerm, setSearchTerm] = useState('');
+export default function NavBar({ onLogout }) {
   const navigate = useNavigate();
-  // const userData = useContext(AppContext);
+  const user = useContext(AppContext);
   const [photoURL, setPhotoURL] = useState('https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg')
 
   const handleClick = () => {
@@ -21,10 +19,10 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
   };
 
   useEffect(() => {
-    if (user?.photoURL) {
-      setPhotoURL(user.photoURL);
+    if (user?.userData?.photoURL) {
+      setPhotoURL(user.userData.photoURL);
     }
-  }, [user])
+  }, [user?.userData?.photoURL])
 
   //This is used for the mobile nav
   const handleLinkClick = () => {
@@ -35,29 +33,12 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
     }
   };
 
-  // const handleSearch = () => {
-  //   navigate('/search-results', { state: { searchTerm: searchTerm } });
-  //   setSearchTerm('');
-  // }
-
-  // const handleEnterKeyPress = (e) => {
-  //   if (e.key === 'Enter') {
-  //     handleSearch()
-  //     setSearchTerm('');
-  //   }
-  // };
-
   return (
     <nav className='sticky top-4 z-[50]'>
       <div id='navbar' className="navbar rounded-xl flex flex-row shadow-lg">
         <div className="basis-1/4">
           <img src={icon} className='max-h-14' id='icon' alt="Chatter Logo" onClick={() => { navigate("/") }} />
         </div>
-        {/* <div className='basis-2/4 gap-2 search justify-center'>
-          <input type='search' name='main-search' id='main-posts-search' value={searchTerm}
-            onKeyDown={handleEnterKeyPress} onChange={(e) => setSearchTerm(e.target.value)} className='input input-bordered w-full max-w-xl' placeholder="What are you searching for today?" />
-          <button onClick={handleSearch} className='btn btn-outline' id='search-button'><i className="fa-solid fa-magnifying-glass"></i></button>
-        </div> */}
         <div className='basis-full justify-end hidden 2xl:flex'>
           <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
@@ -68,7 +49,7 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
               {user === null && <li><Link to='/login'>Login</Link></li>}
             </ul>
           </div>
-          {user !== null && loggedUserHandle && (
+          {user !== null && (
             <div className="dropdown dropdown-end">
               <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
@@ -76,8 +57,6 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
                 </div>
               </label>
               <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow-md bg-neutral-50 rounded-box w-52">
-                {/*<li onClick={handleClick}><Link to={`/users/${loggedUserHandle}`}>See Profile</Link></li>
-                <li onClick={handleClick}><Link to={`/users/${loggedUserHandle}/edit`}>Edit Profile</Link></li>*/}
                 <li onClick={handleClick}><Link to={`/app`}>Go to App</Link></li>
                 <li onClick={handleClick}><Link to='/' onClick={onLogout} >Logout</Link></li>
               </ul>
@@ -100,10 +79,8 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
                 <li><Link to='/faq' onClick={handleLinkClick}>FAQs</Link></li>
                 {user === null && <li onClick={handleLinkClick}><Link to='/register'>Register</Link></li>}
                 {user === null && <li onClick={handleLinkClick}><Link to='/login'>Login</Link></li>}
-                {/*user !== null && loggedUserHandle && <li onClick={handleLinkClick}><Link to={`/users/${loggedUserHandle}`}>See Profile</Link></li>}
-                {user !== null && loggedUserHandle && <li onClick={handleLinkClick}><Link to={`/users/${loggedUserHandle}/edit`}>Edit Profile</Link></li>*/}
-                <li onClick={handleClick}><Link to={`/app/insights`}>Go to App</Link></li>
-                {user !== null && loggedUserHandle && <li onClick={handleLinkClick}><Link to='/' onClick={onLogout}>Logout</Link></li>}
+                {user === null && <li onClick={handleClick}><Link to={`/app`}>Go to App</Link></li>}
+                {user !== null && <li onClick={handleLinkClick}><Link to='/' onClick={onLogout}>Logout</Link></li>}
               </ul>
             </div>
           </div>
@@ -114,7 +91,5 @@ export default function NavBar({ user, onLogout, loggedUserHandle }) {
 }
 
 NavBar.propTypes = {
-  user: PropTypes.object,
   onLogout: PropTypes.func,
-  loggedUserHandle: PropTypes.string,
 };
