f680021c31017be288388d46bb2bda7467dfae70
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-02 16:56:50 +0200

    Improve ChatDetails component
    Added new components StatusBubble and GroupChatAvatar
    
    Introduced the ability to view chat details with new button
    
    Changed look of component for better user experience

--- collab-messenger/src/components/ChatDetails/ChatDetails.jsx
+++ collab-messenger/src/components/ChatDetails/ChatDetails.jsx
@@ -9,20 +9,25 @@ import AppContext from '../../context/AuthContext';
 import { db } from '../../config/firebase-config';
 import { ref, onValue, query, orderByChild, equalTo, update } from 'firebase/database';
 import Avatar from '../Avatar/Avatar';
-import LeaveChatModal from '../LeaveChatModal/LeaveChatModal';
 import ReactGiphySearchbox from 'react-giphy-searchbox-techedge';
 import UploadFile from '../UploadFile/UploadFile';
+import StatusBubble from '../StatusBubble/StatusBubble';
+import ChatInformation from '../ChatInformation/ChatInformation';
+import GroupChatAvatar from '../GroupChatAvatar/GroupChatAvatar';
 
 export default function ChatDetails() {
   const loggedUser = useContext(AppContext);
+  const loggedUserHandle = loggedUser.userData?.handle;
   const { chatId } = useParams();
   const [isGifSearchVisible, setIsGifSearchVisible] = useState(false);
+  const [isChatInfoVisible, setIsChatInfoVisible] = useState(false);
+  const layout = isChatInfoVisible === true ? 'basis-9/12' : 'basis-12/12';
   const [chatData, setChatData] = useState({
     title: '',
     messages: {},
-    participants: []
+    participants: {}
   });
-  const chatTitle = chatData?.isGroup === false ? Object.keys(chatData.participants).find(user => user !== loggedUser?.userData?.handle) : chatData.title;
+  const chatTitle = !chatData?.isGroup ? Object.keys(chatData.participants).find(user => user !== loggedUserHandle) : chatData.title;
   const [allMessages, setAllMessages] = useState([]);
   const [message, setMessage] = useState({
     author: '',
@@ -63,9 +68,9 @@ export default function ChatDetails() {
       return;
     }
 
-    createMessage(message.content, loggedUser.userData.handle, chatId)
+    createMessage(message.content, loggedUserHandle, chatId)
       .then((messageId) => {
-        return addMessage(loggedUser.userData.handle, chatId, messageId, message.content)
+        return addMessage(loggedUserHandle, chatId, messageId, message.content)
       })
       .then(() => {
         setMessage((prevMessage) => ({
@@ -75,7 +80,7 @@ export default function ChatDetails() {
       })
       .catch((error) => {
         toast('Cannot create message. Please try again.')
-        console.log('Error in creating message', error.message);
+        console.log('Error in creating message: ', error.message);
       })
   };
 
@@ -83,16 +88,17 @@ export default function ChatDetails() {
     getChatById(chatId)
       .then((data) => {
         setChatData(data);
+
         if (data.messages) {
-          setAllMessages(Object.keys(data.messages))
+          setAllMessages(Object.keys(data.messages));
         } else {
           setAllMessages([]);
         }
       })
       .catch((e) => {
         toast('Error in getting chat data. Please try again.')
-        console.log(e.message);
-      })
+        console.log('Error in getting chat data: ', e.message);
+      });
     
       const chatRef = ref(db, `chats/${chatId}`);
       const chatListener = onValue(chatRef, (snapshot) => {
@@ -101,22 +107,22 @@ export default function ChatDetails() {
           setChatData(updatedChatData);
 
           if (updatedChatData.messages) {
-            setAllMessages(Object.keys(updatedChatData.messages))
+            setAllMessages(Object.keys(updatedChatData.messages));
           }
         }
         
         if (updatedChatData?.participantsReadMsg) {
-          const loggedUserLastReadMessage = updatedChatData.participantsReadMsg[loggedUser.userData.handle];
+          const loggedUserLastReadMessage = updatedChatData.participantsReadMsg[loggedUserHandle];
           const isLatestMessage = loggedUserLastReadMessage === updatedChatData.lastMessage;
 
           if (!isLatestMessage){
             const updateUserLastReadMessage = () => {
               const updateMsg = {};
-              updateMsg[`/chats/${chatId}/participantsReadMsg/${loggedUser.userData.handle}`] = updatedChatData.lastMessage;
+              updateMsg[`/chats/${chatId}/participantsReadMsg/${loggedUserHandle}`] = updatedChatData.lastMessage;
 
               return update(ref(db), updateMsg)
                 .catch((e) =>
-                  console.log(`Error adding last read message to logged user data`, e.message)
+                  console.log(`Error adding last read message to logged user data: `, e.message)
                 );
             };
             updateUserLastReadMessage();
@@ -130,61 +136,67 @@ export default function ChatDetails() {
       const messagesListener = onValue(chatMessagesQuery, (snapshot) => {
         const updatedMessages = snapshot.val();
         if(updatedMessages) {
-          setAllMessages(Object.keys(updatedMessages))
+          setAllMessages(Object.keys(updatedMessages));
         }
-      })
+      });
   
       return () => {
         chatListener();
         messagesListener();
       };
-  }, [chatId, loggedUser.userData?.handle]);
+  }, [chatId, loggedUserHandle]);
 
   return (
-    <div id='chat-details-wrapper' className="w-full h-[90vh] overflow-y-auto bg-w [&::-webkit-scrollbar]:[width:8px]
-      [&::-webkit-scrollbar-thumb]:bg-lightBlue [&::-webkit-scrollbar-thumb]:rounded-md p-1">
-      <div id="header" className="sticky w-full flex border-b-[0.5px] sm:px-4 py-3 lg:px-6 justify-between items-center shadow-sm">
-        <div id="header-content" className="flex gap-3 items-center">
-          <div id='chat-avatar' className="flex flex-row gap-3">
-            {isGroupChat === true ? (
-              <div className="chat-image avatar w-10 h-10">
-                <div className="rounded-full">
-                  <img src={'https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg'} />
+    <div id='chatDetails-wrapper' className='flex flex-row w-full gap-3'>
+      <div id='chat-section-layout' className={`${layout} w-full rounded-md bg-white`}>
+        <div id="header" className="sticky w-full pt-3 flex sm:px-4 py-3 lg:px-6 justify-between items-center shadow-sm">
+          <div id="header-content" className="flex gap-3 items-center">
+            <div id='chat-avatar' className="flex flex-row gap-3">
+              {isGroupChat === true ? (
+                <GroupChatAvatar />
+              ) : (
+                <div>
+                  <Avatar user={chatTitle} isGroup={chatData?.isGroup} />
+                  <StatusBubble view={'ChatDetails'} userHandle={chatTitle} />
                 </div>
-              </div>
-            ) : (
-              <Avatar user={chatTitle} />
-              // {/* <div className="flex flex-col text-sm font-light text-neutral-500">Active</div> */}
-            )}
-            <div id='chat-title' className="flex place-items-end">{chatTitle}</div> 
+              )}
+              <div id='chat-title' className="flex place-items-end">{chatTitle}</div> 
+            </div>
+          </div>
+          <div className='flex justify-end gap-1'>
+            <button className="btn btn-ghost btn-sm" onClick={() => setIsChatInfoVisible(!isChatInfoVisible)}><i className="fa-solid fa-ellipsis text-pink"></i></button>
           </div>
         </div>
-        {isGroupChat && <LeaveChatModal chatId={chatId} />}
-      </div>
-      <div id='messages-wrapper' className="p-4 h-[70vh] overflow-auto [&::-webkit-scrollbar]:[width:8px]
-          [&::-webkit-scrollbar-thumb]:bg-mint [&::-webkit-scrollbar-thumb]:rounded-md p-1">
-        {areThereMessages === true ? (
-          <MessagesList chatMessages={allMessages} />
-        ) : (
-          <p className="self-center">Start a conversation</p>
+        <div id='messages-wrapper' className="p-4 h-[68vh] overflow-auto [&::-webkit-scrollbar]:[width:8px]
+            [&::-webkit-scrollbar-thumb]:bg-lightBlue [&::-webkit-scrollbar-thumb]:rounded-md p-1">
+          {areThereMessages === true ? (
+            <MessagesList chatMessages={allMessages} />
+          ) : (
+            <p className="self-center">Start a conversation</p>
+          )}
+        </div>
+        <div id='chat-options' className='sticky py-2 px-4 bg-transparent border-t flex items-center gap-2 lg:gap-4 w-full'>
+          <span id='search-gifs-option' className="hover:cursor-pointer hover:bg-grey btn btn-sm text-black bg-transparent flex items-center w-fit " onClick={() => setIsGifSearchVisible(!isGifSearchVisible)}>GIF</span>
+          <UploadFile message={message} setMessageFunc={setMessage} />
+          <textarea id='add-message-option' className="text-black bg-grey font-light py-2 px-4 w-full h-10 rounded-full focus:outline-none" placeholder="Type a message" value={isLink ? '' : message.content} onChange={updateMessage('content')}></textarea>
+          <button id='send-message-option' onClick={onReply} className="p-2w-full rounded-full bg-lightBlue cursor-pointer hover:bg-sky-600 transition"><i className="fa-regular fa-paper-plane"></i></button>
+        </div>
+        {isGifSearchVisible === true && (
+          <div id='giphy-searchbox-wrapper' className='fixed bottom-24'>
+            <ReactGiphySearchbox
+              apiKey="Iy7WxBnblvFgo3jx4SFOIte0fBIDKY0X" 
+              onSelect={gif => addGif(gif.images.fixed_height.url)}
+              rating
+              wrapperClassName=''
+              searchFormClassName='bg-blue text-black w-[270px]'
+              listWrapperClassName='w-[270px] border-[0.5px]'
+            />
+          </div>
         )}
       </div>
-      <div id='chat-options' className='sticky py-2 px-4 bg-transparent border-t flex items-center gap-2 lg:gap-4 w-full'>
-        <span id='search-gifs-option' className="hover:cursor-pointer hover:bg-mint btn btn-sm text-black bg-transparent flex items-center w-fit " onClick={() => setIsGifSearchVisible(!isGifSearchVisible)}>GIF</span>
-        <UploadFile message={message} setMessageFunc={setMessage} />
-        <textarea id='add-message-option' className="text-black bg-white font-light py-2 px-4 bg-neutral-100 w-full h-10 rounded-full focus:outline-none" placeholder="Type a message" value={isLink ? '' : message.content} onChange={updateMessage('content')}></textarea>
-        {<button id='send-message-option' onClick={onReply} className="p-2w-full rounded-full bg-mint cursor-pointer hover:bg-sky-600 transition"><i className="fa-regular fa-paper-plane"></i></button>}
-      </div>
-      {isGifSearchVisible === true && (
-        <div id='giphy-searchbox-wrapper' className='fixed bottom-24'>
-          <ReactGiphySearchbox
-            apiKey="Iy7WxBnblvFgo3jx4SFOIte0fBIDKY0X" 
-            onSelect={gif => addGif(gif.images.fixed_height.url)}
-            rating
-            wrapperClassName=''
-            searchFormClassName='bg-blue text-black w-[270px]'
-            listWrapperClassName='w-[270px] border-[0.5px]'
-          />
+      {isChatInfoVisible === true && (
+        <div id='chatInformation-section-layout' className={`basis-3/12 bg-white w-full rounded-md`}>
+          <ChatInformation isGroupChat={isGroupChat} chatTitle={chatTitle} chatId={chatId} chatData={chatData} />
         </div>
       )}
     </div>
