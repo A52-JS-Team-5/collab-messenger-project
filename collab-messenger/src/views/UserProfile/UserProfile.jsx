596952f4a041db24af118c8d46b63beaeebc2a7a
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-02 17:34:35 +0200

    Update UserProfile
    - Rewrote UserProfile, updating the logic.
    - Transformed it into a modal instead of a standalone page.
    - Styled the modal to align with the overall app design.

--- collab-messenger/src/views/UserProfile/UserProfile.jsx
+++ collab-messenger/src/views/UserProfile/UserProfile.jsx
@@ -1,74 +1,110 @@
 import { useEffect, useState } from 'react';
 import { getUserByHandle } from '../../services/users.services';
-import UserDetails from '../../components/UserDetails/UserDetails';
-import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { db } from '../../config/firebase-config';
+import { onValue, ref } from 'firebase/database';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
 
-const UserProfile = () => {
-  const { userHandle } = useParams();
-
+const UserProfile = ({ userHandle, isOpen, onClose }) => {
   const [userDetails, setUserDetails] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     getUserByHandle(userHandle)
       .then((userData) => {
-        setUserDetails(userData.val());
+        setUserDetails(userData);
         setLoading(false);
       })
       .catch((error) => {
         toast('Cannot load user profile. Please try again later.');
         console.error('Error fetching user data: ', error);
-        setError(error);
         setLoading(false);
       });
+
+    const userRef = ref(db, `users/${userHandle}`);
+    const userListener = onValue(userRef, (snapshot) => {
+      const updatedUserData = snapshot.val();
+      if (updatedUserData) {
+        setUserDetails(updatedUserData);
+      }
+    })
+
+    return () => {
+      userListener();
+    };
+
   }, [userHandle]);
 
-  const [activeTab, setActiveTab] = useState(0); // 0 for Posts, 1 for Comments, 2 for Liked Posts
-
-  const handleTabClick = (tabIndex) => {
-    setActiveTab(tabIndex);
+  const timestamp = new Date(userDetails?.createdOn).toLocaleDateString();
+  const defaultPhotoURL = 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg';
+  const statusColors = {
+    'Online': 'bg-green',
+    'Offline': 'bg-black',
+    'Away': 'bg-yellow'
   };
 
+  const modalClass = cn({
+    "modal modal-bottom sm:modal-middle": true,
+    "modal-open": isOpen,
+  });
+
   return (
-    <div className='flex px-40 max-xl:px-6 py-12 min-h-[90vh]'>
-      {!loading && userDetails &&
-        <div className='flex flex flex-row max-2xl:flex-col gap-10 w-full'>
-          <div className='basis-1/4 max-xl:basis-4/4'>
-            <UserDetails user={userDetails} />
-          </div>
-          <div className='user-content basis-3/4 flex flex-col gap-4 w-full'>
-            <div className='profile-actions'>
-              <div className="tabs max-sm:block">
-                <a className={`tab tab-bordered  max-sm:block ${activeTab === 0 && 'tab-active'}`} onClick={() => handleTabClick(0)}>Posts by the User</a>
-                <a className={`tab tab-bordered  max-sm:block ${activeTab === 1 && 'tab-active'}`} onClick={() => handleTabClick(1)}>Comments by the User</a>
-                <a className={`tab tab-bordered  max-sm:block ${activeTab === 2 && 'tab-active'}`} onClick={() => handleTabClick(2)}>Liked Posts</a>
+    !loading && userDetails &&
+    <div className="user-profile-wrapper">
+      <div id='user-profile-modal' className={`${modalClass}`}>
+        <div className="modal-box bg-gradient-to-b from-pureWhite from-20% to-lightBlue to-20% min-[320px]:max-w-2xl max-[4800px]:max-w-2xl">
+          <button className="btn btn-sm btn-circle hover:bg-lightBlue border-none text-blue bg-pureWhite absolute right-2 top-2" onClick={onClose}>✕</button>
+          <div className='flex flex-col gap-10'>
+            <div className='flex flex-col'>
+              <div className='flex flex-col gap-6'>
+                <div className="avatar">
+                  {userDetails?.status && <div className={`absolute left-[70px] bottom-0 w-4 h-4 rounded-full ${statusColors[userDetails?.status]} border-2 border-white z-40`}></div>}
+                  <div className="w-24 rounded-full aspect-square bg-pureWhite">
+                    <img src={userDetails?.photoURL || defaultPhotoURL} alt="Avatar" className="w-24 h-24 rounded-full aspect-square border-4 border-pureWhite" />
+                  </div>
+                </div>
+                <div className='flex flex-col w-full rounded-md bg-pureWhite p-6'>
+                  <div className='flex flex-col w-full'>
+                    <h2 className='text-xl font-bold text-left'>{userDetails.name} {userDetails.surname}</h2>
+                    <p className='text-left'>@{userDetails.handle}</p>
+                  </div>
+                  <div className="divider w-full"></div>
+                  {userDetails?.bio &&
+                    <div className='flex flex-col'>
+                      <p className='text-left text-blue text-sm'>Bio</p>
+                      <p className='text-left'>{userDetails.bio}</p>
+                      <div className="divider"></div>
+                    </div>}
+                  <div className='flex flex-row justify-between w-full max-md:flex-col gap-2'>
+                    <div className='flex flex-col'>
+                      <p className='text-left text-blue text-sm'>Email</p>
+                      <p className='text-left'>{userDetails.email}</p>
+                    </div>
+                    <div className='flex flex-col'>
+                      <p className='text-left text-blue text-sm'>Phone Number</p>
+                      <p className='text-left'>{userDetails.phoneNumber}</p>
+                    </div>
+                    <div className='flex flex-col'>
+                      <p className='text-left text-blue text-sm'>Member since</p>
+                      <p className='text-left'>{timestamp}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className='flex profile-actions-content w-full'>
-              {activeTab === 0 }
-              {activeTab === 1 }
-              {activeTab === 2 }
-            </div>
           </div>
         </div>
-      }
-      {!loading && error && (
-        <div className="alert alert-error">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          <span>{error}</span>
-        </div>
-      )}
-      {!userDetails && !loading && (
-        <div className="alert alert-error">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-          <span>User Not Found.</span>
-        </div>
-      )}
+      </div>
     </div>
   );
+};
+
+UserProfile.propTypes = {
+  userHandle: PropTypes.string,
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func
 };
 
 export default UserProfile;
