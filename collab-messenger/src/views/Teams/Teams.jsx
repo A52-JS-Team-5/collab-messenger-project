02b5396a8899455dba7fb53566d2aa318ac97a04
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-02 18:04:42 +0200

    Refactor Teams
    - Rewrote the logic for team` data retrieval, aiming for efficiency with fewer queries.
    - Partially improved the component`s styling.

--- collab-messenger/src/views/Teams/Teams.jsx
+++ collab-messenger/src/views/Teams/Teams.jsx
@@ -1,33 +1,90 @@
 import { useContext, useEffect, useState } from "react";
 import CreateTeam from "../../components/CreateTeam/CreateTeam";
 import AppContext from "../../context/AuthContext";
-import { getTeamsByUser } from "../../services/teams.services";
 import EmptyList from "../../components/EmptyList/EmptyList";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { onValue, ref } from "firebase/database";
+import { db } from "../../config/firebase-config";
 
 const Teams = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const user = useContext(AppContext);
+    const { userData } = useContext(AppContext);
     const [teams, setTeams] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (user?.userData?.handle) {
-            getTeamsByUser(user.userData.handle)
-                .then((allTeams) => {
-                    const existingTeams = allTeams.filter((t) => t !== 'Team is Deleted');
-                    setTeams(existingTeams);
+        if (userData?.handle) {
+            const userRef = ref(db, `users/${userData.handle}/teamsMember`);
+
+            // Listen for changes in teamsMember
+            const teamsMemberListener = onValue(userRef, (snapshot) => {
+                if (!snapshot.exists()) {
+                    setTeams([]);
                     setIsLoading(false);
-                })
-                .catch(e => {
-                    console.log('Error getting teams', e.message);
-                    setIsLoading(false);
-                    toast('An error occurred while trying to get the teams you are part of.')
-                })
+                    return;
+                }
+
+                const teamsMember = snapshot.val();
+                const updatedTeams = [];
+
+                // Create an array of promises for fetching team data
+                const promises = Object.keys(teamsMember).map((key) => {
+                    const teamRef = ref(db, `teams/${key}`);
+
+                    // Listen for changes in individual teams
+                    const teamListener = onValue(teamRef, (teamSnapshot) => {
+                        const team = teamSnapshot.val();
+
+                        if (team === null) {
+                            // Update existing team data
+                            const updatedTeamIndex = updatedTeams.findIndex(t => t.id === key);
+                            if (updatedTeamIndex !== -1) {
+                                updatedTeams.splice(updatedTeamIndex, 1, { id: key, message: 'Team is Deleted' });
+                            }
+                        } else {
+                            const updatedTeam = {
+                                id: key,
+                                name: team.name,
+                                createdOn: team.createdOn,
+                                owner: team.owner,
+                                members: team.members ? Object.keys(team.members) : [],
+                                channels: team.channels ? Object.keys(team.channels) : [],
+                                photoURL: team.photoURL,
+                                description: team.description,
+                            };
+
+                            // Update or add the team in the list
+                            const existingTeamIndex = updatedTeams.findIndex(t => t.id === key);
+                            if (existingTeamIndex !== -1) {
+                                updatedTeams.splice(existingTeamIndex, 1, updatedTeam);
+                            } else {
+                                updatedTeams.push(updatedTeam);
+                            }
+                        }
+
+                        // Update state with the latest teams
+                        setTeams(updatedTeams);
+                        setIsLoading(false);
+                    });
+
+                    return teamListener;
+                });
+
+                // Wait for all promises to resolve
+                return Promise.all(promises)
+                    .catch(() => {
+                        setIsLoading(false);
+                        toast('An error occurred while trying to get the teams you are part of.')
+                    });
+            });
+
+            // Return the listener so it can be used to unsubscribe if needed
+            return teamsMemberListener;
         }
-    }, [user]);
+
+    }, [userData?.handle]);
 
     return (
         <div className='mt-4'>
@@ -36,7 +93,7 @@ const Teams = () => {
                 <CreateTeam />
             </div>
             {!isLoading &&
-                <div className='mt-4 w-full flex flex-row justify-between h-[79vh] overflow-y-auto [&::-webkit-scrollbar]:[width:8px]
+                <div className='mt-4 flex flex-row justify-between h-[79vh] items-start overflow-y-auto [&::-webkit-scrollbar]:[width:8px]
                 [&::-webkit-scrollbar-thumb]:bg-lightBlue [&::-webkit-scrollbar-thumb]:rounded-md p-1'>
                     {teams.length > 0 ? (
                         <div className='grid grid-cols-5 max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 max-xl:grid-cols-4 gap-4 w-full'>
