baf24451b66c9720b873789ca390051767303589
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-03 16:37:56 +0200

    Refactor App logic
    - Fixed the useEffect and updated dependencies.
    - Replaced window.location.pathname with location.pathname and added useLocation hook.
    - Moved BrowserRouter and ToastContainer to main.jsx.

--- collab-messenger/src/App.jsx
+++ collab-messenger/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react'
 import AppContext from './context/AuthContext';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { logoutUser } from './services/auth.services';
 import { changeStatus, getUserData } from './services/users.services';
-import { auth } from './config/firebase-config';
+import { auth, db } from './config/firebase-config';
 import Home from './views/Home/Home';
 import About from './views/About/About';
 import NavBar from './components/NavBar/NavBar';
 import Register from './views/Register/Register';
 import Login from './views/Login/Login';
-import { ToastContainer } from 'react-toastify';
 import EditUserProfile from './views/EditUserProfile/EditUserProfile';
 import Footer from './components/Footer/Footer';
 import PageNotFound from './views/PageNotFound/PageNotFound';
@@ -26,9 +25,11 @@ import SearchResults from './views/SearchResults/SearchResults';
 import ChatDetails from './components/ChatDetails/ChatDetails';
 import MobileSideMenu from './components/MobileSideMenu/MobileSideMenu';
 import ChannelDetails from './components/ChannelDetails/ChannelDetails'
+import { equalTo, onValue, orderByChild, query, ref } from 'firebase/database';
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
+  const location = useLocation();
 
   const [appState, setAppState] = useState({
     user,
@@ -62,73 +63,85 @@ function App() {
           throw new Error('Something went wrong!');
         }
 
-        setAppState({
-          ...appState,
+        setAppState(prevState => ({
+          ...prevState,
           userData: snapshot.val()[Object.keys(snapshot.val())[0]],
-        });
+        }));
       })
       .catch(e => {
         alert(e.message);
       })
-  }, [user, appState]);
+
+    const userQuery = query(ref(db, 'users'), orderByChild('uid'), equalTo(user.uid));
+    const userListener = onValue(userQuery, (snapshot) => {
+      if (!snapshot.exists()) {
+        throw new Error('Something went wrong!');
+      }
+
+      setAppState(prevState => ({
+        ...prevState,
+        userData: snapshot.val()[Object.keys(snapshot.val())[0]],
+      }));
+    })
+
+    return () => userListener();
+
+  }, [user]);
 
   return (
-    <BrowserRouter>
-      <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
-        {window.location.pathname.includes('/app') ? (
-          <div className='flex flex-row gap-4 max-h-[96vh] overflow-hidden'>
-            {/* If we're in the website part, we use the flex flex-col className, else - flex flex-row */}
-            {!loading && <SideMenu />}
-            {/* Conditional rendering based on user, loading, and error */}
-            {loading && <div>Loading...</div>}
-            {error && <div>Error: {error.message}</div>}
-            {!loading && !error && (
-              <div className='flex flex-col flex-1 min-h-[96vh]'>
-                {<AppNav onLogout={onLogout} />}
-                <Routes>
-                  <Route path="/app/users/:userHandle/edit" element={<EditUserProfile />} />
-                  <Route path='*' element={<PageNotFound />} />
-                  <Route path='/app/search-results' element={<SearchResults />} />
-                  <Route path="/app" element={<Insights />} />
-                  <Route path='/app/chats' element={<ChatsLayout />} >
-                    <Route path=':chatId' element={<ChatDetails />} />
-                  </Route>
-                  <Route path="/app/teams" element={<Teams />} />
-                  <Route path="/app/teams/:teamId" element={<SingleTeamView />} >
-                    <Route path=':channelId' element={<ChannelDetails />} />
-                  </Route>
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              </div>
-            )}
-            {!loading && !error && <MobileSideMenu />}
-          </div>
-        ) : (
-          <div className='flex flex-col'>
-            {/* If we're in the website part, we use the flex flex-col className, else - flex flex-row */}
-            {!loading && <NavBar onLogout={onLogout} />}
-            {/* Conditional rendering based on user, loading, and error */}
-            {loading && <div>Loading...</div>}
-            {error && <div>Error: {error.message}</div>}
-            {!loading && !error && (
-              <div className='flex flex-col flex-1 min-h-[96vh]'>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path='*' element={<PageNotFound />} />
-                  <Route path="/faq" element={<Faqs />} />
-                  <Route path="/app" element={<Insights />} />
-                </Routes>
-              </div>
-            )}
-            {!loading && !error && <Footer />}
-          </div>
-        )}
-      </AppContext.Provider >
-      <ToastContainer />
-    </BrowserRouter>
+    <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
+      {location.pathname.includes('/app') ? (
+        <div className='flex flex-row gap-4 max-h-[96vh] overflow-hidden'>
+          {/* If we're in the website part, we use the flex flex-col className, else - flex flex-row */}
+          {!loading && <SideMenu />}
+          {/* Conditional rendering based on user, loading, and error */}
+          {loading && <div>Loading...</div>}
+          {error && <div>Error: {error.message}</div>}
+          {!loading && !error && (
+            <div className='flex flex-col flex-1 min-h-[96vh]'>
+              {<AppNav onLogout={onLogout} />}
+              <Routes>
+                <Route path="/app/users/:userHandle/edit" element={<EditUserProfile />} />
+                <Route path='*' element={<PageNotFound />} />
+                <Route path='/app/search-results' element={<SearchResults />} />
+                <Route path="/app" element={<Insights />} />
+                <Route path='/app/chats' element={<ChatsLayout />} >
+                  <Route path=':chatId' element={<ChatDetails />} />
+                </Route>
+                <Route path="/app/teams" element={<Teams />} />
+                <Route path="/app/teams/:teamId" element={<SingleTeamView />} >
+                  <Route path=':channelId' element={<ChannelDetails />} />
+                </Route>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </div>
+          )}
+          {!loading && !error && <MobileSideMenu />}
+        </div>
+      ) : (
+        <div className='flex flex-col'>
+          {/* If we're in the website part, we use the flex flex-col className, else - flex flex-row */}
+          {!loading && <NavBar onLogout={onLogout} />}
+          {/* Conditional rendering based on user, loading, and error */}
+          {loading && <div>Loading...</div>}
+          {error && <div>Error: {error.message}</div>}
+          {!loading && !error && (
+            <div className='flex flex-col flex-1 min-h-[96vh]'>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<About />} />
+                <Route path='*' element={<PageNotFound />} />
+                <Route path="/faq" element={<Faqs />} />
+                <Route path="/app" element={<Insights />} />
+              </Routes>
+            </div>
+          )}
+          {!loading && !error && <Footer />}
+        </div>
+      )}
+    </AppContext.Provider >
   )
 }
 
