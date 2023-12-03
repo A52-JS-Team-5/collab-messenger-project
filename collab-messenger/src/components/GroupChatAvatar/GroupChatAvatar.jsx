ef0eb60ebdd06dadd94dfedbd0957d557e800a3d
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-03 20:28:38 +0200

    Change styling of GroupChatAvatar
    Style is changed based on which component the avatar is used in

--- collab-messenger/src/components/GroupChatAvatar/GroupChatAvatar.jsx
+++ collab-messenger/src/components/GroupChatAvatar/GroupChatAvatar.jsx
@@ -1,11 +1,16 @@
+import PropTypes from 'prop-types';
 import GroupChatDefaultAvatar from '../../assets/GroupChatDefaultAvatar.png';
 
-export default function GroupChatAvatar() {
+export default function GroupChatAvatar({ chatComponent='' }) {
   return (
-    <div className="chat-image avatar w-10 h-10">
+    <div className={`chat-image static avatar ${chatComponent === 'ChatInformation' ? 'w-24 h-24' : 'w-10 h-10'}`}>
       <div className="rounded-full">
         <img src={GroupChatDefaultAvatar} />
       </div>
     </div>
   )
+}
+
+GroupChatAvatar.propTypes = {
+  chatComponent: PropTypes.string
 }
