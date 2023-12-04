66701f8d25034d204df7fb4de42c371795a1d2da
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-04 20:32:43 +0200

    Make SingleTeamView responsive
    - Utilized react-responsive for handling responsiveness.
    - Refactored userChannels logic in the useEffect.
    - Introduced state and functions for mobile view.
    - Introduced EnterChannel component.

--- collab-messenger/src/views/SingleTeamView/SingleTeamView.jsx
+++ collab-messenger/src/views/SingleTeamView/SingleTeamView.jsx
@@ -9,10 +9,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import TeamDetails from "../../components/TeamDetails/TeamDetails";
 import LeaveTeamModal from "../../components/LeaveTeamModal/LeaveTeamModal";
 import DeleteTeamModal from "../../components/DeleteTeamModal/DeleteTeamModal";
-import PageNotFound from "../PageNotFound/PageNotFound";
 import ChannelBox from "../../components/ChannelBox/ChannelBox";
 import { Outlet } from "react-router-dom";
 import AddChannelModal from "../../components/AddChannelModal/AddChannelModal";
+import { useMediaQuery } from 'react-responsive';
+import EnterChannel from "../../components/EnterChannel/EnterChannel";
 
 const SingleTeamView = () => {
 
@@ -21,9 +22,8 @@ const SingleTeamView = () => {
     const [isLoading, setIsLoading] = useState(true);
     const user = useContext(AppContext);
     const navigate = useNavigate();
-    const [showManageTeam, setShowManageTeam] = useState('');
-    const [activeComponent, setActiveComponent] = useState(0); // 0 for ChannelView, 1 for TeamDetails
-    const [isDeleted, setIsDeleted] = useState(false);
+    const [showManageTeam, setShowManageTeam] = useState(false);
+    const [activeComponent, setActiveComponent] = useState(0); // 0 for Default, 1 for TeamDetails, 2 for ChannelView
     const [allTeamChannelsOfUser, setAllTeamChannelsOfUser] = useState([]);
 
     useEffect(() => {
@@ -31,25 +31,23 @@ const SingleTeamView = () => {
 
         getTeamById(teamId)
             .then((teamData) => {
-                if (teamData) {
-                    setTeamDetails(teamData);
-                    setIsLoading(false);
+                setTeamDetails(teamData);
+                setIsLoading(false);
 
-                    // add logic for filtering channels that the user is in with team channels
-                    const userChannels = teamData.channels.filter((channel) => Object.keys(user.userData?.channels).includes(channel));
-                    setAllTeamChannelsOfUser(userChannels);
+                // add logic for filtering channels that the user is in with team channels
+                const userChannels = (teamData.channels || []).filter((channel) =>
+                    Object.keys(user.userData?.channels || {}).includes(channel)
+                );
+                setAllTeamChannelsOfUser(userChannels);
 
-                    // Check if the current user is the owner
-                    if (teamData.owner === user.userData?.handle) {
-                        setShowManageTeam(true);
-                    }
-                } else {
-                    setIsDeleted(true);
+                // Check if the current user is the owner
+                if (teamData.owner === user?.userData?.handle) {
+                    setShowManageTeam(true);
                 }
             })
             .catch((error) => {
                 setIsLoading(false);
-                console.log('Error fetching post', error.message);
+                console.log('Error fetching details', error.message);
                 toast('An error occurred while trying to get the team details.')
             });
 
@@ -84,9 +82,25 @@ const SingleTeamView = () => {
         }
     };
 
-    const handleListClick = (tabIndex) => {
-        setActiveComponent(tabIndex);
+    // State and functions that set the active component
+    const { channelId } = useParams();
+    useEffect(() => {
+        if (channelId) {
+            setActiveMobileComponent(2);
+            setActiveComponent(2);
+        } else {
+            setActiveMobileComponent(0);
+        }
+    }, [channelId]);
+
+    const handleClickTeamDetails = () => {
+        setActiveComponent(1);
         handleClick();
+        navigate(`/app/teams/${teamId}`);
+    };
+
+    const handleClickChannelBox = () => {
+        setActiveComponent(2);
     };
 
     // State and functions for LeaveTeamModal
@@ -117,63 +131,144 @@ const SingleTeamView = () => {
     const openCreateChannelModal = () => {
         setIsCreateChannelModalOpen(true);
     }
-    
+
     const closeCreateChannelModal = () => {
         setIsCreateChannelModalOpen(false);
-    }    
+    }
+
+    // Responsiveness
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 1224px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
+    // Mobile functions and states
+    const [activeMobileComponent, setActiveMobileComponent] = useState(''); // 0 for Default View, 1 for TeamDetails, 2 for Channel
+
+    const handleMobileClickTeamDetails = () => {
+        setActiveMobileComponent(1);
+        handleClick();
+        navigate(`/app/teams/${teamId}`);
+    };
+
+    const handleMobileClickChannelBox = () => {
+        setActiveMobileComponent(2);
+    };
+
+    const handleMobileDefaultClick = () => {
+        setActiveMobileComponent(0);
+        navigate(`/app/teams/${teamId}`);
+    }
 
     return (
-        <div className='flex flex-row mt-4 gap-4 h-[90vh]'>
-            {!isLoading && !isDeleted && (
-                <div className='flex flex-row mt-4 gap-4 h-full'>
-                    <div className='flex flex-col basis-1/5 p-4 rounded-md bg-pureWhite'>
-                        <div className='flex flex-col gap-2'>
-                            <div className='flex flex-row items-center'>
-                                <i className="fa-solid fa-chevron-left fa-xs"></i>
-                                <div className='btn btn-link pl-1 pr-1 pt-0 pb-0 mt-0 mb-0' onClick={() => navigate(-1)}>
-                                    All Teams
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-4 w-[38vh]'>
-                                <div className="place-self-center">
-                                    <img src={teamDetails?.photoURL} className='w-28 h-28 object-cover rounded-md'></img>
-                                </div>
-                                <div className='flex flex-row items-center justify-between'>
-                                    <h2 className='text-xl text-left font-bold'>{teamDetails?.name}</h2>
-                                    <div className="dropdown dropdown-end">
-                                        <label tabIndex="0" className="btn btn-ghost btn-square btn-sm text-blue hover:bg-blue30 focus:!bg-blue30">
-                                            <i className="fa-solid fa-ellipsis-vertical"></i>
-                                        </label>
-                                        <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow-md bg-pureWhite rounded-box w-52">
-                                            <li onClick={() => handleListClick(1)}><a>Team Details</a></li>
-                                            <li onClick={openCreateChannelModal}><a>Add Channel</a></li>
-                                            {/*Leave - only for members who are not the owner*/}
-                                            {!showManageTeam && <li onClick={openLeaveModal}><a>Leave Team</a></li>}
-                                            {/*Delete - only for team owner*/}
-                                            {showManageTeam && <li onClick={openDeleteTeamModal}><a>Delete Team</a></li>}
-                                        </ul>
-                                        {isLeaveModalOpen && (<LeaveTeamModal teamId={teamId} isOpen={isLeaveModalOpen} onClose={closeLeaveModal} />)}
-                                        {isDeleteTeamModalOpen && (<DeleteTeamModal teamData={teamDetails} teamId={teamId} isOpen={isDeleteTeamModalOpen} onClose={closeDeleteTeamModal} />)}
+        <>
+            {isDesktopOrLaptop && <div className='flex flex-row mt-4 gap-4 h-[87vh]'>
+                {!isLoading && (
+                    <div className='flex flex-row mt-4 gap-4 h-full w-full'>
+                        <div className='flex flex-col basis-1/5 p-4 rounded-md bg-pureWhite'>
+                            <div className='flex flex-col gap-2'>
+                                <div className='flex flex-row items-center'>
+                                    <i className="fa-solid fa-chevron-left fa-xs"></i>
+                                    <div className='btn btn-link pl-1 pr-1 pt-0 pb-0 mt-0 mb-0' onClick={() => navigate('/app/teams')}>
+                                        All Teams
                                     </div>
-                                    {isCreateChannelModalOpen && (<AddChannelModal teamId={teamId} teamParticipants={teamDetails.members} teamOwner={teamDetails.owner} isOpen={isCreateChannelModalOpen} onClose={closeCreateChannelModal} teamName={teamDetails.name}/>)}
+                                </div>
+                                <div className='flex flex-col gap-4'>
+                                    <img src={teamDetails?.photoURL} className='w-24 h-24 object-cover rounded-md'></img>
+                                    <div className='flex flex-row items-center justify-between'>
+                                        <h2 className='text-xl text-left font-bold'>{teamDetails?.name}</h2>
+                                        <div className="dropdown dropdown-end">
+                                            <label tabIndex="0" className="btn btn-ghost btn-square btn-sm text-blue hover:bg-blue30 focus:!bg-blue30">
+                                                <i className="fa-solid fa-ellipsis-vertical"></i>
+                                            </label>
+                                            <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow-md bg-pureWhite rounded-box w-52">
+                                                <li onClick={handleClickTeamDetails}><a>Team Details</a></li>
+                                                <li onClick={openCreateChannelModal}><a>Add Channel</a></li>
+                                                {/*Leave - only for members who are not the owner*/}
+                                                {!showManageTeam && <li onClick={openLeaveModal}><a>Leave Team</a></li>}
+                                                {/*Delete - only for team owner*/}
+                                                {showManageTeam && <li onClick={openDeleteTeamModal}><a>Delete Team</a></li>}
+                                            </ul>
+                                            {isLeaveModalOpen && (<LeaveTeamModal teamId={teamId} isOpen={isLeaveModalOpen} onClose={closeLeaveModal} />)}
+                                            {isDeleteTeamModalOpen && (<DeleteTeamModal teamData={teamDetails} teamId={teamId} isOpen={isDeleteTeamModalOpen} onClose={closeDeleteTeamModal} />)}
+                                            {isCreateChannelModalOpen && (<AddChannelModal teamId={teamId} teamParticipants={teamDetails.members} teamOwner={teamDetails.owner} isOpen={isCreateChannelModalOpen} onClose={closeCreateChannelModal} teamName={teamDetails.name} />)}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="divider"></div>
+                            <div className="flex flex-col gap-1 h-[32vh] overflow-y-auto [&::-webkit-scrollbar]:[width:8px]
+                [&::-webkit-scrollbar-thumb]:bg-lightBlue [&::-webkit-scrollbar-thumb]:rounded-md p-1">
+                                {allTeamChannelsOfUser.map(channel => (
+                                    <ChannelBox key={channel} channelId={channel} onClick={handleClickChannelBox} />)
+                                )}
+                            </div>
                         </div>
-                        <div className="divider"></div>
-                        <div className="flex flex-col gap-1 h-[32vh] overflow-y-auto">
-                            {allTeamChannelsOfUser.map(channel => (
-                            <ChannelBox key={channel} channelId={channel}/>)
-                            )}
+                        {activeComponent === 0 && <div className="basis-4/5 w-full flex items-center place-content-evenly overflow-auto p-4">
+                            <EnterChannel />
+                        </div>}
+                        {activeComponent === 2 && <div className="basis-4/5 w-full flex items-center place-content-evenly overflow-auto p-4 rounded-md bg-pureWhite">
+                            <Outlet />
+                        </div>}
+                        {activeComponent === 1 && <TeamDetails teamDetails={teamDetails} showManageTeam={showManageTeam} />}
+                    </div>
+                )}
+            </div >}
+            {/*Mobile and Tablet Views and Logic*/}
+            {isTabletOrMobile && <div className='flex flex-row mt-4 gap-4 max-h-[86vh] w-full'>
+                {!isLoading && activeMobileComponent === 0 && (
+                    <div className='flex flex-row mt-4 gap-4 h-full w-full'>
+                        <div className='flex flex-col p-4 rounded-md bg-pureWhite w-full'>
+                            <div className='flex flex-col gap-2'>
+                                <div className='flex flex-row items-center'>
+                                    <i className="fa-solid fa-chevron-left fa-xs"></i>
+                                    <div className='btn btn-link pl-1 pr-1 pt-0 pb-0 mt-0 mb-0' onClick={() => navigate('/app/teams')}>
+                                        All Teams
+                                    </div>
+                                </div>
+                                <div className='flex flex-col gap-4'>
+                                    <img src={teamDetails?.photoURL} className='w-24 h-24 object-cover rounded-md'></img>
+                                    <div className='flex flex-row items-center justify-between'>
+                                        <h2 className='text-xl text-left font-bold'>{teamDetails?.name}</h2>
+                                        <div className="dropdown dropdown-end">
+                                            <label tabIndex="0" className="btn btn-ghost btn-square btn-sm text-blue hover:bg-blue30 focus:!bg-blue30">
+                                                <i className="fa-solid fa-ellipsis-vertical"></i>
+                                            </label>
+                                            <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow-md bg-pureWhite rounded-box w-52">
+                                                <li onClick={handleMobileClickTeamDetails}><a>Team Details</a></li>
+                                                <li onClick={openCreateChannelModal}><a>Add Channel</a></li>
+                                                {/*Leave - only for members who are not the owner*/}
+                                                {!showManageTeam && <li onClick={openLeaveModal}><a>Leave Team</a></li>}
+                                                {/*Delete - only for team owner*/}
+                                                {showManageTeam && <li onClick={openDeleteTeamModal}><a>Delete Team</a></li>}
+                                            </ul>
+                                            {isLeaveModalOpen && (<LeaveTeamModal teamId={teamId} isOpen={isLeaveModalOpen} onClose={closeLeaveModal} />)}
+                                            {isDeleteTeamModalOpen && (<DeleteTeamModal teamData={teamDetails} teamId={teamId} isOpen={isDeleteTeamModalOpen} onClose={closeDeleteTeamModal} />)}
+                                            {isCreateChannelModalOpen && (<AddChannelModal teamId={teamId} teamParticipants={teamDetails.members} teamOwner={teamDetails.owner} isOpen={isCreateChannelModalOpen} onClose={closeCreateChannelModal} teamName={teamDetails.name} />)}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="flex flex-col gap-1 h-[32vh] overflow-y-auto [&::-webkit-scrollbar]:[width:8px]
+                [&::-webkit-scrollbar-thumb]:bg-lightBlue [&::-webkit-scrollbar-thumb]:rounded-md p-1">
+                                {allTeamChannelsOfUser.map(channel => (
+                                    <ChannelBox key={channel} channelId={channel} onClick={handleMobileClickChannelBox} />)
+                                )}
+                            </div>
                         </div>
                     </div>
-                    <div className="basis-11/12 w-full h-[92vh] flex items-center place-content-evenly overflow-auto">
-                        <Outlet />
+                )}
+                {!isLoading && activeMobileComponent === 2 && <div className="!basis-full max-xl:basis-4/5 w-full flex flex-col items-start place-content-evenly overflow-auto p-4 rounded-md bg-pureWhite">
+                    <div className='flex flex-row items-center xl:hidden'>
+                        <i className="fa-solid fa-chevron-left fa-xs"></i>
+                        <div className='flex flex-col w-full items-start btn btn-link pl-1 pr-1 pt-0 pb-0 mt-0 mb-0' onClick={handleMobileDefaultClick}>Return To Team</div>
                     </div>
-                    {activeComponent === 1 && <TeamDetails teamDetails={teamDetails} showManageTeam={showManageTeam} />}
-                </div>
-            )}
-            {isDeleted && <PageNotFound />}
-        </div >
+                    <Outlet />
+                </div>}
+                {!isLoading && activeMobileComponent === 1 && <TeamDetails teamDetails={teamDetails} showManageTeam={showManageTeam} onClick={handleMobileDefaultClick} />}
+            </div >}
+        </>
     )
 }
 
