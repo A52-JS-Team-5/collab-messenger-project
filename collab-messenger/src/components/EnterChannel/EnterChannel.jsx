b170bfe80aa4ea9c7ef43a949f6ae1a3e14975a2
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-04 19:55:06 +0200

    Introduce EnterChannel component
    Guides users to start chatting by clicking on a channel from the list.

---
+++ collab-messenger/src/components/EnterChannel/EnterChannel.jsx
@@ -0,0 +1,16 @@
+import StartChatting from '../../assets/start-chatting/start-chatting.svg'
+
+const EnterChannel = () => {
+
+    return (
+        <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center'>
+            <img src={StartChatting} id='EmptyList' alt="Empty List Illustration" className='max-h-64' />
+            <div className='flex flex-col gap-2'>
+                <h1 className='text-xl font-bold'>Ready to dive into conversation?</h1>
+                <p>Just click on a channel to kick off the chat!</p>
+            </div>
+        </div>
+    )
+}
+
+export default EnterChannel;
