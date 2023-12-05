d843a3b8f0f8a188e9e6032614834d0f2ba21f42
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 10:38:40 +0200

    Add dark styles in Footer

--- collab-messenger/src/components/Footer/Footer.jsx
+++ collab-messenger/src/components/Footer/Footer.jsx
@@ -20,17 +20,17 @@ export default function Footer() {
   
   return (
     <div className="bottom-0">
-      <div className="hero my-8 bg-grey">
+      <div className="hero my-8">
         <div className="hero-content flex-col w-full">
           <div className='w-full'>
-            <div className='card bg-base-100 flex-col md:flex-row items-center justify-between'>
+            <div className='card bg-base-100 flex-col md:flex-row items-center justify-between dark:bg-darkAccent'>
               <div className='text md:w-2/3 p-4 text-white'>
                 <h1 className='text-left'>Medium length heading</h1>
                 <p className='text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
               </div>
             <div className='flex items-center justify-center md:justify-end md:w-1/3 p-4 md:pl-0 md:pr-4'>
-              <button className="bg-pink text-black mr-2" onClick={() => navigate('/register')}>Register</button>
-              <button className="bg-darker-blue text-white" onClick={() => navigate('/login')}>Login</button>
+              <button className="bg-pink text-pureWhite mr-2" onClick={() => navigate('/register')}>Register</button>
+              <button className="bg-blue text-pureWhite" onClick={() => navigate('/login')}>Login</button>
             </div>
           </div>
         </div>
@@ -41,11 +41,11 @@ export default function Footer() {
           <img src={icon} className='w-10' alt="Chatter logo" />
         </div>
         <div className='pl-14 flex flex-col md:flex-row md:items-center text-black space-y-4 md:space-y-0 md:space-x-4'>
-          <ul onClick={handleLinkClick}><Link to='/'>Home</Link></ul>
-          <ul onClick={handleLinkClick}><Link to='/about'>About</Link></ul>
-          <ul onClick={handleLinkClick}><Link to='/faq'>FAQs</Link></ul>
-          <ul onClick={handleLinkClick}><Link to='/login'>Login</Link></ul>
-          <ul onClick={handleLinkClick}><Link to='/register'>Register</Link></ul>
+          <ul onClick={handleLinkClick}><Link to='/' className='dark:text-yellow'>Home</Link></ul>
+          <ul onClick={handleLinkClick}><Link to='/about' className='dark:text-yellow'>About</Link></ul>
+          <ul onClick={handleLinkClick}><Link to='/faq' className='dark:text-yellow'>FAQs</Link></ul>
+          <ul onClick={handleLinkClick}><Link to='/login' className='dark:text-yellow'>Login</Link></ul>
+          <ul onClick={handleLinkClick}><Link to='/register' className='dark:text-yellow'>Register</Link></ul>
         </div>
         <div className='flex space-x-2 other-logos'>
           <img src={fb} alt="Facebook logo" />
@@ -58,8 +58,8 @@ export default function Footer() {
       <br />
       <hr className="text-black"/>
       <br />
-      <div className='text-black flex flex-col md:flex-row md:items-center md:justify-center space-y-2 md:space-y-0 md:space-x-4'>
-        <p className="text-black">&copy; Assemble 2023 - All rights reserved</p>
+      <div className='text-black flex flex-col md:flex-row md:items-center md:justify-center space-y-2 md:space-y-0 md:space-x-4 dark:text-darkText'>
+        <p className="text-black dark:text-darkText">&copy; Assemble 2023 - All rights reserved</p>
         {/* to add onClick events for the respective pages */}
         <p>Privacy Policy</p> 
         <p>Terms of Service</p>
