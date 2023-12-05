f1c3fd6c59843377deed381b3f8577165ec0ed1e
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-05 19:59:43 +0200

    Add ChannelsList component
    This component is needed for showing the list of channels of a team
    
    This list is included in the Team Details View

---
+++ collab-messenger/src/components/ChannelsList/ChannelsList.jsx
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { deleteChannel, getChannelById } from '../../services/channels.services';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AddChannelModal from '../AddChannelModal/AddChannelModal';
+
+export default function ChannelsList({ teamDetails }) {
+  const navigate = useNavigate();
+  const [allChannels, setAllChannels] = useState([]);
+  const [isCreateChannelModalOpen, setIsCreateChannelModalOpen] = useState(false);
+
+  const openCreateChannelModal = () => {
+    setIsCreateChannelModalOpen(true);
+  };
+
+  const closeCreateChannelModal = () => {
+    setIsCreateChannelModalOpen(false);
+  };
+
+  useEffect(() => {
+    const getChannelDetails = () => {
+      if (teamDetails.channels) {
+        const channelPromises = teamDetails.channels.map((channelId) =>
+          getChannelById(channelId)
+            .then((snapshot) => {
+              const channelData = snapshot;
+              return { id: channelId, ...channelData };
+            })
+            .catch((error) => {
+              console.error(`Error fetching channel data ${channelId}: ${error.message}`);
+            })
+          );
+
+          Promise.all(channelPromises)
+            .then((channelsDataArray) => {
+              setAllChannels(channelsDataArray);
+            })
+            .catch((error) => console.error(`Error fetching channel details: ${error.message}`));
+      }
+    };
+
+    getChannelDetails();
+}, [teamDetails.channels]);
+
+  const handleChannelDeletion = (channelData) => {
+
+    deleteChannel(channelData)
+      .then(() => {
+        navigate(`/app/teams/${channelData.team}`);
+        toast('Channel was deleted successfully');
+      })
+      .catch((e) => console.log(e.message));
+  };
+
+  return (
+    <div className='flex flex-col gap-2'>
+      <div className="flex flex-col bg-white rounded-md p-4">
+        <div className='flex flex-col items-end mb-3'>
+          <button onClick={openCreateChannelModal} className='btn bg-pink border-none text-pureWhite w-fit justify-center'><i className="fa-solid fa-users"></i>Add Channel</button>
+          {isCreateChannelModalOpen && (
+            <AddChannelModal teamId={teamDetails.id} teamParticipants={teamDetails.members} teamOwner={teamDetails.owner} isOpen={isCreateChannelModalOpen} onClose={closeCreateChannelModal} teamName={teamDetails.name}/>
+          )}
+        </div>
+        {allChannels.map(channel => (
+          <li key={channel.id} className='flex flex-row justify-between gap-2 p-2 mt-1 rounded-md items-center cursor-pointer hover:bg-pureWhite'>
+            <div className='flex flex-row gap-2 items-center justify-between w-full'>
+              <div className='flex flex-row w-full'>
+                <p className='place-self-center ' onClick={() => navigate(`/app/teams/${channel.team}/${channel.id}`)}>{channel.title}</p>
+              </div>
+              {channel.title !== 'General' && (
+                <button onClick={() => handleChannelDeletion(channel)} className="btn btn-ghost btn-sm btn-square hover:!bg-grey text-blue">
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              )}
+            </div>
+          </li>)
+        )}
+      </div>
+    </div>
+  )
+}
+
+ChannelsList.propTypes = {
+  teamDetails: PropTypes.object,
+};
