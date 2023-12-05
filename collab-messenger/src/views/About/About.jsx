63c3556c7511910379859581d6c1266acdb94758
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 10:57:11 +0200

    Add dark styles in About

--- collab-messenger/src/views/About/About.jsx
+++ collab-messenger/src/views/About/About.jsx
@@ -19,57 +19,57 @@ function About() {
 
   return (
     <>
-      <div className="hero my-12 py-10 bg-grey">
+      <div className="hero my-12 py-10">
         <div className="hero-content flex-col lg:flex-row-reverse gap-14">
             <div className='flex flex-col items-start max-lg:items-center max-w-2xl'>
-                <h1 className="text-5xl font-bold text-black text-center max-lg:text-center">Describe why your company exists [mission statement]</h1>
-                <p className="py-6 text-black text-center max-lg:text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+                <h1 className="text-5xl font-bold text-black text-center max-lg:text-center dark:text-darkText">Describe why your company exists [mission statement]</h1>
+                <p className="py-6 text-black text-center max-lg:text-center dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
             </div>
         </div>
       </div>
-      <div className="hero my-12 py-10 bg-white">
-        <div className="hero-content flex-col lg:flex-row-reverse gap-14">
+      <div className="hero my-12 py-10  gap-14 bg-grey dark:bg-darkFront">
+        <div className="hero-content flex-col lg:flex-row-reverse">
             <img src={image} className="max-w-md rounded-lg" />
             <div className='flex flex-col items-start max-lg:items-center max-w-2xl'>
-              <h1 className="text-5xl font-bold text-black text-left max-lg:text-center">About [company name]</h1>
-              <p className="py-6 text-black text-left max-lg:text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque. Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae.</p>
-              <p className="py-6 text-black text-left max-lg:text-center">Amet pellentesque sit pulvinar lorem mi a, euismod risus rhoncus. Elementum ullamcorper nec, habitasse vulputate. Eget dictum quis est sed egestas tellus, a lectus. Quam ullamcorper in fringilla arcu aliquet fames arcu.Lacinia eget faucibus urna, nam risus nec elementum cras porta. Sed elementum, sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit sagittis in eleifend dignissim. Natoque mauris cras molestie velit. Maecenas eget adipiscing quisque viverra lectus arcu, tincidunt ultrices pellentesque.</p>
+              <h1 className="text-5xl font-bold text-black text-left max-lg:text-center dark:text-darkText">About [company name]</h1>
+              <p className="py-6 text-black text-left max-lg:text-center dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque. Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae.</p>
+              <p className="py-6 text-black text-left max-lg:text-center dark:text-darkText">Amet pellentesque sit pulvinar lorem mi a, euismod risus rhoncus. Elementum ullamcorper nec, habitasse vulputate. Eget dictum quis est sed egestas tellus, a lectus. Quam ullamcorper in fringilla arcu aliquet fames arcu.Lacinia eget faucibus urna, nam risus nec elementum cras porta. Sed elementum, sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit sagittis in eleifend dignissim. Natoque mauris cras molestie velit. Maecenas eget adipiscing quisque viverra lectus arcu, tincidunt ultrices pellentesque.</p>
             </div>
           </div>
         </div>
-        <div className="hero my-8 pt-10 bg-grey">
+        <div className="hero my-8 pt-10">
           <div className="hero-content flex-col lg:flex-row-reverse gap-14">
             <div className='flex flex-col items-start max-lg:items-center max-w-2xl'>
-              <h1 className="text-5xl font-bold text-black text-center max-lg:text-center">Emphasize what&apos;s important to your company</h1>
-              <p className="py-6 text-black text-center max-lg:text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.</p>
+              <h1 className="text-5xl font-bold text-black text-center max-lg:text-center dark:text-darkText">Emphasize what&apos;s important to your company</h1>
+              <p className="py-6 text-black text-center max-lg:text-center dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.</p>
             </div>
           </div>
         </div>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <div className='card text-black'>
+            <div className='card text-black  dark:text-darkText'>
               <h2 className='font-bold'>Highlight Value One</h2>
               <p className='p-4 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.</p>
             </div>
-            <div className='card text-black'>
+            <div className='card text-black  dark:text-darkText'>
               <h2 className='font-bold'>Highlight Value Two</h2>
               <p className='p-4 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.</p>
             </div>
-            <div className='card text-black'>
+            <div className='card text-black  dark:text-darkText'>
               <h2 className='font-bold'>Highlight Value Three</h2>
               <p className='p-4 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.</p>
             </div>
         </div>
-        <div className='p-10 bg-white'>
-                <p className='text-4xl font-bold text-black pb-10'>Get to know the creators</p>
+        <div className='p-10'>
+                <p className='text-4xl font-bold text-black pb-10 dark:text-darkText'>Get to know the creators</p>
                 <div className='flex gap-6 justify-center max-md:flex-col'>
-                    <div className="card max-w-xs bg-grey">
+                    <div className="card max-w-xs bg-white dark:bg-darkAccent">
                         <figure className="px">
                             <img src={image} alt="Silvia Stoykova" className="rounded-xl" />
                         </figure>
                         <div className="items-start text-center pt-5">
-                            <h2 className="card-title text-black pt-2">Silvia Stoykova</h2>
-                            <p className="card-title text-black font-normal pt-2">Job Title</p>
-                            <p className="card-title text-black font-normal text-start pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                            <h2 className="card-title text-black pt-2 dark:text-darkText">Silvia Stoykova</h2>
+                            <p className="card-title text-black font-normal pt-2 dark:text-darkText">Job Title</p>
+                            <p className="card-title text-black font-normal text-start pt-2 dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                         </div>
                         <div className='flex space-x-4 other-logos w-4 pt-4'>
                           <img src={linkedIn} alt="LinkedIn logo" />
@@ -77,14 +77,14 @@ function About() {
                           <img src={dribble} alt="Dribbble logo" />
                         </div>
                     </div>
-                    <div className="card max-w-xs bg-grey">
+                    <div className="card max-w-xs bg-white dark:bg-darkAccent">
                         <figure className="px">
                             <img src={image} alt="Georgi Dobrev" className="rounded-xl" />
                         </figure>
                         <div className="items-start text-center pt-5">
-                            <h2 className="card-title text-black pt-2">Georgi Dobrev</h2>
-                            <p className="card-title text-black font-normal pt-2">Job Title</p>
-                            <p className="card-title text-black font-normal text-start pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                            <h2 className="card-title text-black pt-2 dark:text-darkText">Georgi Dobrev</h2>
+                            <p className="card-title text-black font-normal pt-2 dark:text-darkText">Job Title</p>
+                            <p className="card-title text-black font-normal text-start pt-2 dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                         </div>
                         <div className='flex space-x-4 other-logos w-4 pt-4'>
                           <img src={linkedIn} alt="LinkedIn logo" />
@@ -92,14 +92,14 @@ function About() {
                           <img src={dribble} alt="Dribbble logo" />
                         </div>
                     </div>
-                    <div className="card max-w-xs bg-grey">
+                    <div className="card max-w-xs bg-white dark:bg-darkAccent">
                         <figure className="px">
                             <img src={image} alt="Teodora Gigova" className="rounded-xl" />
                         </figure>
                         <div className="items-start text-center pt-5">
-                            <h2 className="card-title text-black pt-2">Teodora Gigova</h2>
-                            <p className="card-title text-black font-normal pt-2">Job Title</p>
-                            <p className="card-title text-black font-normal text-start pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                            <h2 className="card-title text-black pt-2 dark:text-darkText">Teodora Gigova</h2>
+                            <p className="card-title text-black font-normal pt-2 dark:text-darkText">Job Title</p>
+                            <p className="card-title text-black font-normal text-start pt-2 dark:text-darkText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                         </div>
                         <div className='flex space-x-4 other-logos w-4 pt-4'>
                           <img src={linkedIn} alt="LinkedIn logo" />
