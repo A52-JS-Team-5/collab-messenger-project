f3a35ee26a22193d9132f46cd3a1fb2b3ab7d3ca
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 10:36:47 +0200

    Add dark styles in EmptyList

--- collab-messenger/src/components/EmptyList/EmptyList.jsx
+++ collab-messenger/src/components/EmptyList/EmptyList.jsx
@@ -5,7 +5,7 @@ const EmptyList = () => {
   return (
     <div className='flex flex-col justify-center gap-5 px-24 max-xl:px-6 py-10 mt-4 w-full items-center'>
       <img src={EmptyImg} id='EmptyList' alt="Empty List Illustration" className='max-h-64'/>
-      <h1 className='text-xl font-bold'>Nothing to Show Yet.</h1>
+      <h1 className='text-xl font-bold dark:text-darkText'>Nothing to Show Yet.</h1>
     </div>
   )
 }
