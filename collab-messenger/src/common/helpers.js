90d498c534a2250bc3b940676d853a96a429ca71
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-05 19:49:04 +0200

    Add helper function for fetching uploads in channels
    Function getUploadedFilesInChannel is added
    It is used to fetch uploaded files in the channels

--- collab-messenger/src/common/helpers.js
+++ collab-messenger/src/common/helpers.js
@@ -1,3 +1,5 @@
+import { ref, list, getStorage, getDownloadURL } from "firebase/storage";
+
 export const isEmailValid = (email) => {
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return emailRegex.test(email);
@@ -6,4 +8,26 @@ export const isEmailValid = (email) => {
 export const isPhoneNumberValid = (number) => {
   const numberRegex = /^\d+$/;
   return numberRegex.test(number);
+}
+
+export const getUploadedFilesInChannel = (channelId) => {
+  const storage = getStorage();
+  const storageRef = ref(storage, `channel_uploads/${channelId}`);
+
+  return list(storageRef, { maxResults: 10 })
+    .then((result) => {
+      const uploads = result.items;
+      const downloadUrls = uploads.map(item => {
+      return getDownloadURL(ref(storage, item._location.path_))
+        .then((url) => ({
+          fileName: item._location.path_.split('/').pop(),
+          'url': url 
+        }))
+          .catch(e => console.log('Error getting download URL for file: ', e.message))
+        })
+  
+      return Promise.all(downloadUrls)
+        
+      })
+    .catch((e) => console.log(e.message));
 }
