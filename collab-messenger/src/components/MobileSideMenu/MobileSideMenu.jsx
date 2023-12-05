13d19ce70349d76e5ce3dd4fc1b866db2b313c0e
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 10:44:11 +0200

    Add dark styles in MobileSideMenu

--- collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
+++ collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
@@ -15,17 +15,17 @@ const MobileSideMenu = () => {
     };
 
     return (
-        <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-lightBlue rounded-full bottom-4 left-1/2 xl:hidden sm:max-xl:display-block">
+        <div className="fixed z-50 w-full h-16 max-w-xs -translate-x-1/2 bg-lightBlue rounded-full bottom-4 left-1/2 xl:hidden sm:max-xl:display-block dark:bg-darkInput">
             <div className="grid h-full grid-cols-3">
-                <button onClick={() => handleLinkClick('/app')} type="button" className={`${activeLink === '/app' && 'bg-blue text-white'} inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group text-blue`}>
+                <button onClick={() => handleLinkClick('/app')} type="button" className={`${activeLink === '/app' && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group text-blue dark:text-yellow`}>
                     <i className="fa-solid fa-house"></i>
                     <span className="sr-only">Insights</span>
                 </button>
-                <button onClick={() => handleLinkClick('/app/chats')} type="button" className={`${activeLink === '/app/chats' && 'bg-blue text-white'} inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group text-blue`}>
+                <button onClick={() => handleLinkClick('/app/chats')} type="button" className={`${activeLink === '/app/chats' && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group text-blue dark:text-yellow`}>
                     <i className="fa-solid fa-message"></i>
                     <span className="sr-only">Chats</span>
                 </button>
-                <button onClick={() => handleLinkClick('/app/teams')} type="button" className={`${activeLink === '/app/teams' && 'bg-blue text-white'} inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 group text-blue`}>
+                <button onClick={() => handleLinkClick('/app/teams')} type="button" className={`${activeLink === '/app/teams' && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 group text-blue dark:text-yellow`}>
                     <i className="fa-solid fa-users"></i>
                     <span className="sr-only">Teams</span>
                 </button>
