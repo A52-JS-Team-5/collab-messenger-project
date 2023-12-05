99241663700403ad05964984fc57a32645f71c61
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-05 19:46:27 +0200

    Update messages services
    Added a function for file uploading in channels
    
    Deleted unnecessary code
    
    Added messagedChannels property in users documents in the database by editing addMessageChannel service

--- collab-messenger/src/services/messages.services.js
+++ collab-messenger/src/services/messages.services.js
@@ -12,10 +12,11 @@ export const addMessage = (userHandle, chatId, messageId, message) => {
   );
 };
 
-export const addMessageChannel = (channelId, messageId, message) => {
+export const addMessageChannel = (channelId, messageId, message, userHandle) => {
   const updateChannelMessages = {};
   updateChannelMessages[`/channels/${channelId}/messages/${messageId}`] = true;
   updateChannelMessages[`/channels/${channelId}/lastMessage/`] = message;
+  updateChannelMessages[`/users/${userHandle}/messagedChannels/${channelId}`] = true;
 
   return update(ref(db), updateChannelMessages).catch((e) =>
     console.log(`Error in adding message to channel: ${e.message}`)
@@ -90,6 +91,34 @@ export const createChannelMessage = (content, userHandle, channelId) => {
     });
 };
 
+export const createFileUploadMessageForChannels = (
+  content,
+  userHandle,
+  channelId,
+  title,
+  type
+) => {
+  const messagesRef = ref(db, 'messages');
+
+  const newMessage = {
+    content: content,
+    author: userHandle,
+    createdOn: Date.now(),
+    channelId: channelId,
+    reactions: {},
+    title: title,
+    type: type,
+  };
+
+  return push(messagesRef, newMessage)
+    .then((newMessageRef) => {
+      return newMessageRef.key;
+    })
+    .catch((error) => {
+      console.log(`Error creating message: ${error.message}`);
+    });
+};
+
 export const getMessageById = (messageId) => {
   return get(ref(db, `messages/${messageId}`))
     .then((snapshot) => {
@@ -109,93 +138,3 @@ export const editMessage = (messageId, updates) => {
     console.log('Error editing comment: ', e.message);
   });
 };
-
-// const commentAscDateSort = (a, b) => {
-//   if (a.createdOn < b.createdOn) {
-//     return -1;
-//   }
-
-//   if (a.createdOn > b.createdOn) {
-//     return 1;
-//   }
-
-//   return 0;
-// };
-
-// const commentDescDateSort = (a, b) => {
-//   if (a.createdOn > b.createdOn) {
-//     return -1;
-//   }
-
-//   if (a.createdOn < b.createdOn) {
-//     return 1;
-//   }
-
-//   return 0;
-// };
-
-// export const commentSortByDateAsc = (allComments) =>
-//   [...allComments].sort(commentAscDateSort);
-
-// export const commentSortByDateDesc = (allComments) =>
-//   [...allComments].sort(commentDescDateSort);
-
-// export const deleteComment = (commentId, postId, userHandle) => {
-//   return Promise.all([
-//     remove(ref(db, `comments/${commentId}`)),
-//     remove(ref(db, `posts/${postId}/comments/${commentId}`)),
-//     remove(ref(db, `posts/${postId}/commentedBy/${userHandle}`)),
-//     remove(ref(db, `${userHandle}/commentedPost/${postId}`))
-//   ])
-//   .catch((e) => console.log('Error in deleting comment', e.message));
-// };
-
-// export const getCommentsByPost = (postId) => {
-//   return get(query(ref(db, `posts/${postId}/comments`)))
-//     .then((snapshot) => {
-//       if (!snapshot.exists()) return [];
-
-//       return Object.keys(snapshot.val());
-//     })
-//     .catch((e) => console.log('Error getting comments by post', e.message));
-// };
-
-// const fromCommentsDocument = (snapshot) => {
-//   const commentsDocument = snapshot.val();
-
-//   return Object.keys(commentsDocument).map((key) => {
-//     const comment = commentsDocument[key];
-
-//     return {
-//       id: key,
-//       description: comment.description,
-//       author: comment.author,
-//       createdOn: new Date(comment.createdOn),
-//       postId: comment.postId,
-//     };
-//   });
-// };
-
-// export const getAllComments = () => {
-//   return get(ref(db, 'comments'))
-//     .then((snapshot) => {
-//       if (!snapshot.exists()) {
-//         return [];
-//       }
-
-//       return fromCommentsDocument(snapshot);
-//     })
-//     .catch((e) => console.log('Error in getting all comments', e.message));
-// };
-
-// export const getCommentsByAuthor = (handle) => {
-//   return get(query(ref(db, 'comments'), orderByChild('author'), equalTo(handle)))
-//     .then((snapshot) => {
-//       if (!snapshot.exists()) {
-//         return [];
-//       }
-
-//       return fromCommentsDocument(snapshot);
-//     })
-//     .catch((e) => console.log('Error in getting comments by author', e.message));
-// };
