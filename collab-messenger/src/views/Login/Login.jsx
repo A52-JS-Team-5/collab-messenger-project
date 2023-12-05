457ebc7fca18c04eed988fac302974043f588327
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 11:02:18 +0200

    Add dark styles in Login

--- collab-messenger/src/views/Login/Login.jsx
+++ collab-messenger/src/views/Login/Login.jsx
@@ -67,27 +67,27 @@ const Login = () => {
   };
 
   return (
-    <div className="hero min-h-[90vh] px-40 max-2xl:px-2">
+    <div className="hero min-h-[90vh] px-40 max-2xl:px-2 dark:text-darkText">
       <div className="hero-content flex-col lg:flex-row-reverse pt-20 pb-20 gap-16 max-2xl:gap-8">
-        <div className='card rounded border-2 border-black rounded-3xl bg-beige shadow-xl min-w-[50%]'>
+        <div className='card rounded rounded-3xl bg-beige shadow-xl min-w-[50%] dark:bg-darkFront'>
           <div className='form-control flex flex-wrap flex-auto'>
-            <h3 className="text-3xl self-center text-black pb-8 font-bold">Sign In</h3>
+            <h3 className="text-3xl self-center text-black pb-8 font-bold dark:text-darkText">Sign In</h3>
             <label className="label" htmlFor='username'>
-              <span className="label-text text-black pl-3">Username</span>
+              <span className="label-text text-black pl-3 dark:text-darkText">Username</span>
             </label>
-            <input type='text' id='username' placeholder="Type your username" className="input input-bordered w-full bg-white" value={form.username} onChange={updateForm('username')} />
+            <input type='text' id='username' placeholder="Type your username" className="input input-bordered w-full bg-white dark:bg-darkInput" value={form.username} onChange={updateForm('username')} />
             <span className="err-message text-red">{formErrorMsg.username}</span>
             <label className="label" htmlFor='password'>
-              <span className="label-text text-black pl-3">Password</span>
+              <span className="label-text text-black pl-3 dark:text-darkText">Password</span>
             </label>
-            <input type='password' id='password' placeholder="Type your password" className="input input-bordered w-full bg-white" value={form.password} onChange={updateForm('password')} />
+            <input type='password' id='password' placeholder="Type your password" className="input input-bordered w-full bg-white dark:bg-darkInput" value={form.password} onChange={updateForm('password')} />
             <span className="err-message text-red">{formErrorMsg.password}</span>
             <br />
             <div className="text-left pb-6 pl-3 flex space-x-24">
               <p>New to Chatter?</p>
               <Link to='/login'>Create an account</Link>
             </div>
-            <button className="btn bg-black self-center text-white w-1/2" onClick={handleLogin}>Sign In</button>
+            <button className="btn bg-pink self-center border-none text-pureWhite w-1/2" onClick={handleLogin}>Sign In</button>
           </div>
         </div>
       </div>
