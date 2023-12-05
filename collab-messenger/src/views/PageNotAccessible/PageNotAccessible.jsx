f9fe37b9a76d0123bd5c774a333b85944ce2f1a8
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 11:02:36 +0200

    Add dark styles in PageNotAccessible

--- collab-messenger/src/views/PageNotAccessible/PageNotAccessible.jsx
+++ collab-messenger/src/views/PageNotAccessible/PageNotAccessible.jsx
@@ -13,7 +13,7 @@ export default function PageNotAccessible() {
   }, [location.pathname]);
 
   return (
-    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center'>
+    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center dark:text-darkText'>
       <img src={PageNotFoundImage} id='PageNotFound' alt="Page Not Found Illustration" className='max-h-64' />
       <div className='flex flex-col gap-2'>
         <h1 className='text-3xl font-bold'>Oops, looks like this page is a VIP zone!</h1>
