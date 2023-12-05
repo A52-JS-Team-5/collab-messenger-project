2e93e1efb1b0e00a5c04e4a551dc021adf5ded08
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 11:15:31 +0200

    Add dark styles in UserProfile

--- collab-messenger/src/views/UserProfile/UserProfile.jsx
+++ collab-messenger/src/views/UserProfile/UserProfile.jsx
@@ -52,20 +52,20 @@ const UserProfile = ({ userHandle, isOpen, onClose }) => {
 
   return (
     !loading && userDetails &&
-    <div className="user-profile-wrapper">
+    <div className="user-profile-wrapper dark:text-darkText">
       <div id='user-profile-modal' className={`${modalClass}`}>
-        <div className="modal-box bg-gradient-to-b from-pureWhite from-20% to-lightBlue to-20% min-[320px]:max-w-2xl max-[4800px]:max-w-2xl">
-          <button className="btn btn-sm btn-circle hover:bg-lightBlue border-none text-blue bg-pureWhite absolute right-2 top-2" onClick={onClose}>✕</button>
+        <div className="modal-box bg-gradient-to-b from-pureWhite from-20% to-lightBlue to-20% dark:bg-gradient-to-b dark:from-darkBase dark:from-20% dark:to-darkFront dark:to-20% min-[320px]:max-w-2xl max-[4800px]:max-w-2xl">
+          <button className="btn btn-sm btn-circle hover:bg-lightBlue border-none text-blue bg-pureWhite absolute right-2 top-2 dark:bg-darkAccent" onClick={onClose}>✕</button>
           <div className='flex flex-col gap-10'>
             <div className='flex flex-col'>
               <div className='flex flex-col gap-6'>
                 <div className="avatar">
-                  {userDetails?.status && <div className={`absolute left-[70px] bottom-0 w-4 h-4 rounded-full ${statusColors[userDetails?.status]} border-2 border-white z-40`}></div>}
+                  {userDetails?.status && <div className={`absolute left-[70px] bottom-0 w-4 h-4 rounded-full ${statusColors[userDetails?.status]} border-2 border-white z-40 dark:border-darkAccent`}></div>}
                   <div className="w-24 rounded-full aspect-square bg-pureWhite">
                     <img src={userDetails?.photoURL || defaultPhotoURL} alt="Avatar" className="w-24 h-24 rounded-full aspect-square border-4 border-pureWhite" />
                   </div>
                 </div>
-                <div className='flex flex-col w-full rounded-md bg-pureWhite p-6'>
+                <div className='flex flex-col w-full rounded-md bg-pureWhite p-6 dark:bg-darkAccent'>
                   <div className='flex flex-col w-full'>
                     <h2 className='text-xl font-bold text-left'>{userDetails.name} {userDetails.surname}</h2>
                     <p className='text-left'>@{userDetails.handle}</p>
@@ -73,21 +73,21 @@ const UserProfile = ({ userHandle, isOpen, onClose }) => {
                   <div className="divider w-full"></div>
                   {userDetails?.bio &&
                     <div className='flex flex-col'>
-                      <p className='text-left text-blue text-sm'>Bio</p>
+                      <p className='text-left text-blue text-sm dark:text-yellow'>Bio</p>
                       <p className='text-left'>{userDetails.bio}</p>
                       <div className="divider"></div>
                     </div>}
                   <div className='flex flex-row justify-between w-full max-md:flex-col gap-2'>
                     <div className='flex flex-col'>
-                      <p className='text-left text-blue text-sm'>Email</p>
+                      <p className='text-left text-blue text-sm dark:text-yellow'>Email</p>
                       <p className='text-left'>{userDetails.email}</p>
                     </div>
                     <div className='flex flex-col'>
-                      <p className='text-left text-blue text-sm'>Phone Number</p>
+                      <p className='text-left text-blue text-sm dark:text-yellow'>Phone Number</p>
                       <p className='text-left'>{userDetails.phoneNumber}</p>
                     </div>
                     <div className='flex flex-col'>
-                      <p className='text-left text-blue text-sm'>Member since</p>
+                      <p className='text-left text-blue text-sm dark:text-yellow'>Member since</p>
                       <p className='text-left'>{timestamp}</p>
                     </div>
                   </div>
