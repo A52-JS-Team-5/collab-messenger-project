b762c310ebea3bc1f1fb80e624fdefcaad558758
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 10:37:12 +0200

    Add dark styles in EnterChannel

--- collab-messenger/src/components/EnterChannel/EnterChannel.jsx
+++ collab-messenger/src/components/EnterChannel/EnterChannel.jsx
@@ -3,7 +3,7 @@ import StartChatting from '../../assets/start-chatting/start-chatting.svg'
 const EnterChannel = () => {
 
     return (
-        <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center'>
+        <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center dark:text-darkText'>
             <img src={StartChatting} id='EmptyList' alt="Empty List Illustration" className='max-h-64' />
             <div className='flex flex-col gap-2'>
                 <h1 className='text-xl font-bold'>Ready to dive into conversation?</h1>
