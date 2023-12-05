27387b02293f68a39d5c8953823b89e7170a5025
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-05 11:02:53 +0200

    Add dark styles in PageNotFound

--- collab-messenger/src/views/PageNotFound/PageNotFound.jsx
+++ collab-messenger/src/views/PageNotFound/PageNotFound.jsx
@@ -13,7 +13,7 @@ export default function PageNotFound() {
   }, [location.pathname]);
 
   return (
-    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center'>
+    <div className='flex flex-col items-center gap-5 px-40 max-xl:px-6 py-12 h-full w-full justify-center dark:text-darkText'>
       <img src={PageNotFoundImage} id='PageNotFound' alt="Page Not Found Illustration" className='max-h-64' />
       <h1 className='text-3xl font-bold'> Page Not Found </h1>
       <p> We&apos;re sorry, the page you requested could not be found.</p>
