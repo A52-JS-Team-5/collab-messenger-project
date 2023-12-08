94ac16e0b4925951330e705ead2ed6531e052414
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-08 09:59:28 +0200

    Style GroupChatAvatar

--- collab-messenger/src/components/GroupChatAvatar/GroupChatAvatar.jsx
+++ collab-messenger/src/components/GroupChatAvatar/GroupChatAvatar.jsx
@@ -3,7 +3,7 @@ import GroupChatDefaultAvatar from '../../assets/GroupChatDefaultAvatar.png';
 
 export default function GroupChatAvatar({ chatComponent='' }) {
   return (
-    <div className={`chat-image static avatar ${chatComponent === 'ChatInformation' ? 'w-24 h-24' : 'w-10 h-10'}`}>
+    <div className={`chat-image static self-center avatar ${chatComponent === 'ChatInformation' ? 'w-24 h-24 aspect-square' : 'w-10 h-10 aspect-square'}`}>
       <div className="rounded-full">
         <img src={GroupChatDefaultAvatar} />
       </div>
