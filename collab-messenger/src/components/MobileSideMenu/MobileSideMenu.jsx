c56e344b9d78abd2b6128c4ca68fad5630392b5e
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-08 10:07:59 +0200

    Add `Later` link in MobileSideMenu
    - Included a link to the `Saved for Later` section in the mobile side menu

--- collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
+++ collab-messenger/src/components/MobileSideMenu/MobileSideMenu.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 const MobileSideMenu = () => {
     const [activeLink, setActiveLink] = useState('');
-    const navigate = useNavigate();   
+    const navigate = useNavigate();
     const location = useLocation();
 
     useEffect(() => {
@@ -16,7 +16,7 @@ const MobileSideMenu = () => {
 
     return (
         <div className="fixed z-50 w-full h-16 max-w-xs -translate-x-1/2 bg-lightBlue rounded-full bottom-4 left-1/2 xl:hidden sm:max-xl:display-block dark:bg-darkInput">
-            <div className="grid h-full grid-cols-3">
+            <div className="grid h-full grid-cols-4">
                 <button onClick={() => handleLinkClick('/app')} type="button" className={`${activeLink === '/app' && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 group text-blue dark:text-yellow`}>
                     <i className="fa-solid fa-house"></i>
                     <span className="sr-only">Insights</span>
@@ -25,9 +25,13 @@ const MobileSideMenu = () => {
                     <i className="fa-solid fa-message"></i>
                     <span className="sr-only">Chats</span>
                 </button>
-                <button onClick={() => handleLinkClick('/app/teams')} type="button" className={`${activeLink.includes('/app/teams') && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 group text-blue dark:text-yellow`}>
+                <button onClick={() => handleLinkClick('/app/teams')} type="button" className={`${activeLink.includes('/app/teams') && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 group text-blue dark:text-yellow`}>
                     <i className="fa-solid fa-users"></i>
                     <span className="sr-only">Teams</span>
+                </button>
+                <button onClick={() => handleLinkClick('/app/later')} type="button" className={`${activeLink.includes('/app/later') && 'bg-blue text-white dark:bg-darkFront'} inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 group text-blue dark:text-yellow`}>
+                    <i className="fa-solid fa-bookmark"></i>
+                    <span className="sr-only">Later</span>
                 </button>
             </div>
         </div>
