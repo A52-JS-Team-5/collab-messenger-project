ca08248d17f20aafad9b50397c0f319f7093b43c
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-09 18:17:09 +0200

    Update EmptyList to accept props

--- collab-messenger/src/components/EmptyList/EmptyList.jsx
+++ collab-messenger/src/components/EmptyList/EmptyList.jsx
@@ -1,13 +1,18 @@
-import EmptyImg from '../../assets/empty-icon/EmptyList.svg'
+import EmptyImg from '../../assets/empty-icon/EmptyList.svg';
+import PropTypes from 'prop-types';
 
-const EmptyList = () => {
+const EmptyList = ({ content }) => {
 
   return (
     <div className='flex flex-col justify-center gap-5 px-24 max-xl:px-6 py-10 mt-4 w-full items-center'>
-      <img src={EmptyImg} id='EmptyList' alt="Empty List Illustration" className='max-h-64'/>
-      <h1 className='text-xl font-bold dark:text-darkText'>Nothing to Show Yet.</h1>
+      <img src={EmptyImg} id='EmptyList' alt="Empty List Illustration" className='max-h-64' />
+      <p className='dark:text-darkText'>{content}</p>
     </div>
   )
 }
+
+EmptyList.propTypes = {
+  content: PropTypes.string,
+};
 
 export default EmptyList;
