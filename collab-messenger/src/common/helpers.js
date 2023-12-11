1a4c1675933f534a7786561a32259ee16794103e
silviastoykova <sylvia.stoykova@gmail.com>
2023-12-11 20:27:46 +0200

    Add function to get uploaded chat files in helpers
    Added a new helper function to use in ChatInformation component
    
    Function gets all uploaded files in chat

--- collab-messenger/src/common/helpers.js
+++ collab-messenger/src/common/helpers.js
@@ -1,5 +1,7 @@
 import { ref, list, getStorage, getDownloadURL } from "firebase/storage";
 
+const storage = getStorage();
+
 export const isEmailValid = (email) => {
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return emailRegex.test(email);
@@ -11,7 +13,6 @@ export const isPhoneNumberValid = (number) => {
 }
 
 export const getUploadedFilesInChannel = (channelId) => {
-  const storage = getStorage();
   const storageRef = ref(storage, `channel_uploads/${channelId}`);
 
   return list(storageRef, { maxResults: 10 })
@@ -30,4 +31,25 @@ export const getUploadedFilesInChannel = (channelId) => {
         
       })
     .catch((e) => console.log(e.message));
+}
+
+export const getUploadedFilesInChat = (chatId) => {
+  const storageRef = ref(storage, `chat_uploads/${chatId}`);
+
+    list(storageRef, { maxResults: 10 })
+      .then((result) => {
+        const uploads = result.items;
+
+        const downloadUrls = uploads.map(item => {
+          return getDownloadURL(ref(storage, item._location.path_))
+            .then((url) => ({
+              fileName: item._location.path_.split('/').pop(),
+              'url': url
+            }))
+            .catch(e => console.log('Error getting download URL for file: ', e.message))
+        })
+
+        return Promise.all(downloadUrls)
+      })
+      .catch((e) => console.log(e.message));
 }
