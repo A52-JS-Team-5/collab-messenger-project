29414ca7f895a9068cde818043a4674e68fb6f99
Teodora Gigova <gigova.teodora@gmail.com>
2023-12-13 04:00:32 +0200

    Update, style, and make `FAQ` page responsive

--- collab-messenger/src/views/FAQs/Faqs.jsx
+++ collab-messenger/src/views/FAQs/Faqs.jsx
@@ -1,64 +1,119 @@
-function Faqs () {
+function Faqs() {
 
-return (
-    <div>
-        <div className="hero my-16">
-            <div className="hero-content flex-col lg:flex-row-reverse gap-14">
-                <div className='flex flex-col items-center max-lg:items-center max-w-2xl'>
-                    <h1 className="text-5xl font-bold text-black max-lg:text-center dark:text-darkText">Frequently asked questions</h1>
-                    <p className="py-6 text-black text-center max-lg:text-center dark:text-darkText">Where collaboration feels effortless, offering intuitive communication and customizable channels. Stay connected, work together, and unleash your team&apos;s creativity with Chatter - your go-to platform for smooth collaboration!</p>
+    return (
+        <div>
+            <div className="hero mt-32">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-14">
+                    <div className='flex flex-col items-center max-lg:items-center max-w-2xl'>
+                        <h1 className="text-5xl font-bold text-black max-lg:text-center dark:text-darkText">Frequently asked questions</h1>
+                        <p className="py-6 text-black text-center max-lg:text-center dark:text-darkText">Where collaboration feels effortless, offering intuitive communication and customizable channels. Stay connected, work together, and unleash your team&apos;s creativity with Chatter - your go-to platform for smooth collaboration!</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='flex flex-col gap-6 px-80 max-xl:px-10 pt-8 pb-8 bg-white text-white dark:bg-darkFront'>
-                <div className="collapse collapse-arrow bg-black dark:bg-darkAccent">
+            <div className='flex flex-col gap-6 px-96 max-xl:px-10 pt-16 pb-16 text-black dark:bg-darkFront dark:text-darkText'>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
                     <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
+                    <div className="collapse-title text-xl text-left font-semibold">
                         <h2>How can I create a team?</h2>
                     </div>
-                    <div className="collapse-content text-center">
-                        <p>To create a team, navigate to the app&apos;s interface and locate the &quot;Create New Team&quot; option. Give your team a name and add members as needed. Once created, this team space becomes a hub for collaboration. Start conversations, share files, and work together seamlessly within this designated team environment.</p>
+                    <div className="collapse-content text-left">
+                        <p>To create a team on Chatter, head to the Teams page. There, you&apos;ll find the &quot;Create New Team&quot; option. Give your team a name and, if you wish, provide a brief description (though this step is optional). Add team members as needed, and voilà — your team space is born.<br/>
+                            Within your newly created team, the &quot;General&quot; channel is automatically set up for you. This channel is your starting point for conversations, file sharing, and seamless collaboration. Dive into the world of teamwork with Chatter and make your communication more effective.</p>
                     </div>
                 </div>
-                <div className="collapse collapse-arrow bg-black dark:bg-darkAccent">
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
                     <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
-                        <h2>Can I have private chats within a team?</h2>
-                    </div>
-                    <div className="collapse-content">
-                        <p>You can maintain confidentiality within a team by creating private channels or initiating direct messages. These private chats enable secure discussions among selected team members, ensuring privacy while collaborating on specific topics or projects.</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-arrow bg-black dark:bg-darkAccent">
-                    <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
+                    <div className="collapse-title text-xl text-left font-semibold">
                         <h2>How can I join an existing team?</h2>
                     </div>
-                    <div className="collapse-content">
-                        <p>To join an existing team, you need an invitation from the team owner. Upon receiving the invitation link or request, follow the provided instructions to join. You might also find the team within the app and request access, awaiting approval from the team owner or admin. Once invited and accepted, you&apos;ll have access to the team&apos;s collaborative spaces and discussions.</p>
+                    <div className="collapse-content text-left">
+                        <p>Currently, only the owner of a team has the capability to add or remove users. While we anticipate future updates that may enhance this process, for now, you should wait to be added by a team member. Sit tight, and you&apos;ll be part of the team in no time.</p>
                     </div>
                 </div>
-                <div className="collapse collapse-arrow bg-black dark:bg-darkAccent">
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
                     <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
-                        <h2>Can I customize the settings within a team?</h2>
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>Can I customize the basic details or members within a team?</h2>
                     </div>
-                    <div className="collapse-content">
-                        <p>The customization of settings and permissions within a team is typically managed by the team owner. As a member, you may have limited control over these configurations. The team owner has the authority to adjust settings, manage permissions, and control access levels for various team functionalities.</p>
+                    <div className="collapse-content text-left">
+                        <p>The customization of details and members within a team is generally overseen by the team owner. As a team member, you don&apos;t have control over these configurations. The team owner has the authority to adjust settings, manage permissions, and control access levels for various team functionalities, including members, channels, and channel members.</p>
                     </div>
                 </div>
-                <div className="collapse collapse-arrow bg-black dark:bg-darkAccent">
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
                     <input type="checkbox" />
-                    <div className="collapse-title text-xl font-medium">
+                    <div className="collapse-title text-xl text-left font-semibold">
                         <h2>Is there a limit to the number of teams I can create?</h2>
                     </div>
-                    <div className="collapse-content">
+                    <div className="collapse-content text-left">
                         <p>There is generally no limit to the number of teams you can create or join within the communication application. You&apos;re free to create multiple teams or join existing ones as per your requirements, enabling seamless collaboration across various projects or groups without any predefined restrictions on team creation or membership.</p>
                     </div>
                 </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>Is there a limit to the number of channels within a team I can create?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>No, there is generally no limit to the number of channels you can create or join within the communication application. You&apos;re free to create multiple channels within a team, whether they&apos;re private or public.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>Can I have private channels within a team?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>Absolutely! Chatter allows you to maintain confidentiality within a team by creating private channels. Additionally, you can initiate direct group messages. These private chats provide a secure space for discussions among selected team members, ensuring privacy while collaborating on specific topics or projects.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>What is the difference between a private and public channel?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>In private channels, the team owner has the authority to select which team members can access the channel and participate. On the other hand, public channels automatically include every member from the team as channel members, allowing for broader participation.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>How can I update my profile?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>To update your profile, click on the avatar in the right corner of the app navigation, then select &quot;Edit Profile&quot; from the dropdown. You&apos;ll be redirected to the Edit Profile page, where you can make changes in three tabs: update basic details, update profile picture, or update password.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>How can I update my status?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>To update your status, click on the avatar in the right corner of the app navigation. From the dropdown selector displaying your current presence, simply choose another status.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>What is the &quot;Later&quot; Page?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>The &quot;Later&quot; page consolidates all the messages you&apos;ve previously saved during your chats. In your chats, you can save and unsave messages by clicking on the bookmark icon. Think of this functionality as your personal favorites, making it easy to revisit and reference important messages.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-white dark:bg-darkAccent">
+                    <input type="checkbox" />
+                    <div className="collapse-title text-xl text-left font-semibold">
+                        <h2>What is the &quot;Insights&quot; Page?</h2>
+                    </div>
+                    <div className="collapse-content text-left">
+                        <p>The &quot;Insights&quot; page serves as your dashboard, gathering notifications about teams and channel activities. Additionally, it provides basic statistics and helpful links tailored to enhance your overall experience and delight.</p>
+                    </div>
+                </div>
             </div>
-    </div>
-)
+        </div>
+    )
 }
 
 export default Faqs
